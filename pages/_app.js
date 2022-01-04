91ef372d58944e19a24ea33cfd2b1ccfacb55ea9
Pushpit <pushpit07@gmail.com>
2022-01-04 17:56:38 +0530

    Basic structure
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import { MoralisProvider } from "react-moralis";
 import Script from "next/script";
-import Layout from "../pageComponents/Layout/Layout";
+// import Layout from "../pageComponents/WrapLayout/Layout";
 
 function App({ Component, pageProps }) {
     const MORALIS_APP_ID = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
@@ -10,6 +10,11 @@ function App({ Component, pageProps }) {
     return (
         <>
             <Script src="https://kit.fontawesome.com/0366dd7992.js" crossorigin="anonymous"></Script>
+            <Script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                crossorigin="anonymous"
+            ></Script>
             <MoralisProvider appId={MORALIS_APP_ID} serverUrl={MORALIS_SERVER_URL}>
                 {/* <Layout> */}
                 <Component {...pageProps} />

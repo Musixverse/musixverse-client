f0345d19fc248c05156b77f0590fc56d1ccf6028
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-01-29 00:26:07 +0530

    Added styling to the Song details component

--- src/components/SongInfo/SongDetails.js
+++ src/components/SongInfo/SongDetails.js
@@ -1,82 +1,86 @@
-import React from "react";
+import Image from "next/image";
 import styles from "../../../styles/SongInfo/SongInfo_details.module.css";
-import data from "../../../metaData";
+import metaData from "../../../metaData";
 
-export default SongDetails = () => {
+export default function SongDetails() {
+  console.log(metaData.id);
+  console.log(metaData.songName);
+  console.log(metaData.artistName);
+  console.log(metaData.price);
   return (
     <>
-      <div className={styles[song-info]}>
-        <div className={styles[song-info__container]}>
-          <div className={styles[song-info__song-details]}>
-            <div className={styles[song-details__title]}>
+      <div className={styles['song-info']}>
+        <div className={styles['song-info__container']}>
+          <div className={styles['song-info__song-details']}>
+            <div className={styles['song-details__title']}>
               <h1>SONG DETAILS</h1>
             </div>
-            <div className={styles[song-details__subtitle]}>
-              <h3>Notes from Owner</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div className={styles['song-details__subtitle']}>
+              <h3 className="font-bold pt-5">Notes from Owner</h3>
+              <p className="pb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div className={styles[song-details__artist-notes]}>
+            <div className={styles['song-details__artist-notes']}>
               <p>
-                {data.Hashes.descHash}
+                {metaData.Hashes.descHash}
               </p>
             </div>
           </div>
-          <div className={styles[song-info__other-details]}>
-            <div className={styles[other-details__title]}>
+          <div className={styles['song-info__other-details']}>
+            <div className={styles['other-details__title']}>
               <h1>OTHER DETAILS</h1>
             </div>
-            <div className={styles[other-details__section1]}>
-              <div>
-                  <h4>Genres</h4>
-                  <p>{data.Characteristics.genre}</p>
+            <div className={styles['other-details__section1']}>
+              <div className="text-left">
+                  <h4 className="font-bold">Genres</h4>
+                  <p>{metaData.Characteristics.genre}</p>
               </div>
-              <div>
-                  <h4>Intrument Used</h4>
-                  <p>{data.instruments}</p>
+              <div className="text-left">
+                  <h4 className="font-bold">Intrument Used</h4>
+                  <p>{metaData.instruments}</p>
               </div>
-              <div>
-                  <h4>sampling</h4>
+              <div className="text-left">
+                  <h4 className="font-bold">sampling</h4>
                   <p>No</p>
               </div>
             </div>
             <hr />
-            <div className={styles[other-details__section2]}>
-              <div>
-                  <h4>Contact Address</h4>
-                  <p>{data.artistAddress}</p>
+            <div className={styles['other-details__section2']}>
+              <div className="text-left">
+                  <h4 className="font-bold">Contact Address</h4>
+                  <p>{metaData.artistAddress}</p>
               </div>
-              <div>
-                  <h4>Token ID</h4>
+              <div className="text-left">
+                  <h4 className="font-bold">Token ID</h4>
                   <p>187372</p>
               </div>
-              <div>
-                  <h4>Token Standard</h4>
+              <div className="text-left">
+                  <h4 className="font-bold">Token Standard</h4>
                   <p>ERC-1176</p>
               </div>
-              <div>
-                  <h4>Blockchain</h4>
+              <div className="text-left">
+                  <h4 className="font-bold">Blockchain</h4>
                   <p>Ethereum</p>
               </div>
             </div>
             <hr />
-            <div className={styles[other-details__section3]}>
-              <div className={styles[section3__artist-name]}>
-                  <h4>{data.artistName}</h4>
-                  <p>Go to artist profile page &nbsp;<i class="fas fa-long-arrow-right"></i></p>
+            <div className={styles['other-details__section3']}>
+              <div className="pt-3">
+                  <h4 className="font-bold">{metaData.artistName}</h4>
+                  <p>Go to artist profile page &nbsp;<span className="align-sub"><Image src="https://img.icons8.com/external-kmg-design-basic-outline-kmg-design/32/000000/external-right-arrow-user-interface-kmg-design-basic-outline-kmg-design.png" width={20} height={20} alt="right arrow"/></span></p>
               </div>
             </div>
-            <div className={styles[other-details__Icons]}>
-              <a href="#"><img src="https://img.icons8.com/ios-filled/50/000000/meta.png"/></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
+            <div className={styles['other-details__Icons']}>
+              <a href="#"><Image src="https://img.icons8.com/ios-filled/50/000000/meta.png" width={20} height={20} alt="meta"/></a>
+              <a href="#"><Image src="https://img.icons8.com/ios-glyphs/30/000000/twitter--v1.png" width={20} height={20} alt="twitter"/></a>
             </div>
-            <div className={styles[other-details__footer]}>
-              <div>
+            <div className={styles['other-details__footer']}>
+              <div className="flex flex-row justify-between ">
                   <h4>Listen on</h4>
-                  <div className={styles[footer__icons]}>
-                      <a href={data.Links.spotify}><img src="https://img.icons8.com/ios-filled/50/000000/spotify.png"/></a>
-                      <a href={data.Links.appleMusic}><img src="https://img.icons8.com/ios-filled/50/000000/apple-music.png"/></a>
-                      <a href={data.Links.amazonMusic}><img src="https://img.icons8.com/color/48/000000/amazon.png"/></a>
-                      <a href={data.Links.youtubeMusic}><img src="https://img.icons8.com/bubbles/50/000000/youtube-music.png"/></a>
+                  <div className={styles['footer__icons']}>
+                      <a href={metaData.Links.spotify}><Image src="https://img.icons8.com/ios-filled/50/000000/spotify.png" width={25} height={25} alt="spotify"/></a>
+                      <a href={metaData.Links.appleMusic}><Image src="https://img.icons8.com/ios-filled/50/000000/apple-music.png" width={25} height={25} alt="apple-music"/></a>
+                      <a href={metaData.Links.amazonMusic}><Image src="https://img.icons8.com/color/48/000000/amazon.png" width={25} height={25} alt="amazon-music"/></a>
+                      <a href={metaData.Links.youtubeMusic}><Image src="https://img.icons8.com/bubbles/50/000000/youtube-music.png" width={25} height={25} alt="youtube-music"/></a>
                   </div>
               </div>
             </div>
--- src/pages/song-info/[id].js
+++ src/pages/song-info/[id].js
@@ -1,9 +1,12 @@
+import SongDetails from "../../components/SongInfo/SongDetails";
 import PurchaseInfo from "../../components/SongInfo/PurchaseInfo";
 
 export default function songInfo() {
     return(
-        <div  className="flex h-screen justify-center items-center text-center">
+        <div  className="flex flex-col justify-center items-center bg-light-200">
+            <SongDetails />
             <PurchaseInfo/>
+
         </div>
     );
 }

f030e7b9e5a326e3f3b39a62eed6287a4a99e050
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-01-28 20:03:32 +0530

    corrected naming in the component

--- src/components/SongInfo/SongDetails.js
+++ src/components/SongInfo/SongDetails.js
@@ -1,30 +1,30 @@
 import React from "react";
-import "../../../styles/SongInfo/SongInfo_details.module.css";
+import styles from "../../../styles/SongInfo/SongInfo_details.module.css";
 import data from "../../../metaData";
 
 export default SongDetails = () => {
   return (
     <>
-      <div className="song-info">
-        <div className="song-info__song-details">
-          <div className="song-details__title">
+      <div className={styles[song-info]}>
+        <div className={styles[song-info__song-details]}>
+          <div className={styles[song-details__title]}>
             <h1>SONG DETAILS</h1>
           </div>
-          <div className="song-details__subtitle">
+          <div className={styles[song-details__subtitle]}>
             <h3>Notes from Owner</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
-          <div className="song-details__artist-notes">
+          <div className={styles[song-details__artist-notes]}>
             <p>
               {data.Hashes.descHash}
             </p>
           </div>
         </div>
-        <div className="song-info__other-details">
-          <div className="other-details__title">
+        <div className={styles[song-info__other-details]}>
+          <div className={styles[other-details__title]}>
             <h1>OTHER DETAILS</h1>
           </div>
-          <div className="other-details__section1">
+          <div className={styles[other-details__section1]}>
             <div>
                 <h4>Genres</h4>
                 <p>{data.Characteristics.genre}</p>
@@ -39,7 +39,7 @@ export default SongDetails = () => {
             </div>
           </div>
           <hr />
-          <div className="other-details__section2">
+          <div className={styles[other-details__section2]}>
             <div>
                 <h4>Contact Address</h4>
                 <p>{data.artistAddress}</p>
@@ -58,20 +58,20 @@ export default SongDetails = () => {
             </div>
           </div>
           <hr />
-          <div className="other-details__section3">
-            <div className="secton3__artist-name">
+          <div className={styles[other-details__section3]}>
+            <div className={styles[section3__artist-name]}>
                 <h4>{data.artistName}</h4>
                 <p>Go to artist profile page &nbsp;<i class="fas fa-long-arrow-right"></i></p>
             </div>
           </div>
-          <div className="other-details__Icons">
+          <div className={styles[other-details__Icons]}>
             <a href="#"><img src="https://img.icons8.com/ios-filled/50/000000/meta.png"/></a>
             <a href="#"><i class="fab fa-twitter"></i></a>
           </div>
-          <div className="other-details__footer">
+          <div className={styles[other-details__footer]}>
             <div>
                 <h4>Listen on</h4>
-                <div className="footer__icons">
+                <div className={styles[footer__icons]}>
                     <a href={data.Links.spotify}><img src="https://img.icons8.com/ios-filled/50/000000/spotify.png"/></a>
                     <a href={data.Links.appleMusic}><img src="https://img.icons8.com/ios-filled/50/000000/apple-music.png"/></a>
                     <a href={data.Links.amazonMusic}><img src="https://img.icons8.com/color/48/000000/amazon.png"/></a>

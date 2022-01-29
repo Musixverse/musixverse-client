72ae1422d28367b97f4c1d32d9847413a4eb69d6
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-01-29 15:48:26 +0530

    Added Newsletter signup

--- src/pages/song-info/[id].js
+++ src/pages/song-info/[id].js
@@ -1,15 +1,20 @@
 import SongDetails from "../../components/SongInfo/SongDetails";
 import PurchaseInfo from "../../components/SongInfo/PurchaseInfo";
 import SalesHistory from "../../components/SongInfo/SalesHistory";
+import { Fragment } from "react";
+import NewsLetter from "../../components/SongInfo/NewsLetter";
 
 export default function songInfo() {
     return(
-        <div  className="flex flex-col justify-center items-center bg-light-200">
-            <SongDetails />
-            <div className="flex w-full justify-center mb-10">
-                <PurchaseInfo/>
-                <SalesHistory/>
+        <Fragment>
+            <div  className="flex flex-col justify-center items-center bg-light-200">
+                <SongDetails />
+                <div className="flex w-full justify-center mb-10">
+                    <PurchaseInfo/>
+                    <SalesHistory/>
+                </div>
             </div>
-        </div>
+            <NewsLetter/>
+        </Fragment>
     );
 }

b7cd367f1c5c0beee311a41276184b8d806ce4a4
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-02-19 16:52:41 +0530

    Made filter section functional

--- src/components/ArtistProfile/Filter.js
+++ src/components/ArtistProfile/Filter.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { useState } from "react";
+import Dropdown from "./ArtistProfileUtils/Dropdown";
 
 export default function Filter(){
     const [currentlyActive, setCurrentlyActive] = useState(1);
@@ -16,15 +17,15 @@ export default function Filter(){
     }
 
     return(
-        <div className="bg-light-300 mt-[85px] p-6 rounded-xl">
+        <div className="bg-light-300 mt-[85px] p-6 rounded-xl flex justify-between items-end">
             {/* Left Section */}
             <div className="flex items-start">
                 <Image src={"/assets/record_b.svg"} alt="vinyl disc" height={30} width={30}></Image>
-                <div className="ml-4 w-1/3">
+                <div className="ml-4 flex-grow">
                     <h4 className="font-tertiary text-[36px] leading-none">RECORDS</h4>
                     <div className="my-1 flex-grow border-t-[2px] border-dark-200"></div>
                     {/* Owned NFTs Category Filters */}
-                    <div className="text-[#5a5a5a] dark:text-light-100">
+                    <div className="text-[#757373] dark:text-light-100">
                         <span className={"text-sm cursor-pointer " + (currentlyActive==1? "text-dark-100 font-semibold":"font-medium")} onClick={handleFilterChange}>All Records</span>
                         <span className={"mx-4 text-sm cursor-pointer " + (currentlyActive==2? "text-dark-100 font-semibold":"font-medium")} onClick={handleFilterChange}>For Sale</span>
                         <span className={"text-sm cursor-pointer " + (currentlyActive==3? "text-dark-100 font-semibold":"font-medium")} onClick={handleFilterChange}>Collections</span>
@@ -32,7 +33,7 @@ export default function Filter(){
                 </div>
             </div>
             {/* Right Section */}
-            {/* A dropdown Filter*/}
+            <Dropdown/>
         </div>
     );
 }

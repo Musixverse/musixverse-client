a4b959edcf6c7e267d8394bdaa00c352ef78933a
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-03-14 18:06:55 +0530

    added Artist and collector page

---
+++ src/pages/register/artist.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Artist_Page = () => {
+    return (
+        <>
+            <Head>
+                <title>Musixverse | Artist</title>
+                <meta name="description" content="Musixverse" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/* Component */}
+        </>
+    );
+};
+
+export default Artist_Page;
---
+++ src/pages/register/collector.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Collector_Page = () => {
+    return (
+        <>
+            <Head>
+                <title>Musixverse | Collector</title>
+                <meta name="description" content="Musixverse" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/* Component */}
+        </>
+    );
+};
+
+export default Collector_Page;

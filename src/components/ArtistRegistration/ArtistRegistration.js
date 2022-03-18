4dea12c75a9edd3ba9d121c395868227e9d99911
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-03-19 01:40:58 +0530

    Developed additional modal

--- src/components/ArtistRegistration/ArtistRegistration.js
+++ src/components/ArtistRegistration/ArtistRegistration.js
@@ -3,8 +3,17 @@ import LeftSection from "./ArtistRegUtils/LeftSection";
 import styles from "../../../styles/ArtistRegistration/Artist.module.css";
 import Button from "./ArtistRegUtils/Button";
 import SelectAvatar from "./ArtistRegUtils/SelectAvatar";
+import { useRef } from "react";
 
 export default function ArtistRegistration(){
+    // ADD REFS TO READ DATA FROM INPUT FIELDS
+    // STATE MIGHT CAUSE MULTIPLE UNECESSARY RE-RENDERS
+    const bio = useRef();
+    const facebook = useRef();
+    const twitter = useRef();
+    const spotify = useRef();
+    const instagram = useRef();
+
     return (
         <div className={styles['register']}>
             <div className={styles['register__container']}>
@@ -26,45 +35,69 @@ export default function ArtistRegistration(){
                     </div>    
                 {/* </div> */}
 
-                {/* Right section */}
+                {/* Right section*/}
                 <RightSection>
                     <p className="text-5xl font-tertiary max-w-[468px] mb-10">YOUR DETAILS</p>
-                    <SelectAvatar/>
-                    {/* <div className="flex-1"> */}
-                        <p className="mt-8 text-[16px] font-secondary font-bold">Your Name</p>
-                        <input type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm" />
-                    {/* </div> */}
-                    <div className="flex flex-col sm:flex-row mt-3 text-[16px] font-secondary font-bold space-y-4 sm:space-x-3 sm:space-y-0">
-                        <div className="flex-1">
-                            <p>Username</p>
-                            <input type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm" />
+                    {/* Step-1 */}
+                    <div className="hidden">
+                        <SelectAvatar/>
+                        {/* <div className="flex-1"> */}
+                            <p className="mt-8 text-[16px] font-secondary font-bold">Your Name</p>
+                            <input type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm outline-none focus:border-primary-100" />
+                        {/* </div> */}
+                        <div className="flex flex-col sm:flex-row mt-3 text-[16px] font-secondary font-bold space-y-4 sm:space-x-3 sm:space-y-0">
+                            <div className="flex-1">
+                                <p>Username</p>
+                                <input type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm outline-none focus:border-primary-100" />
+                            </div>
+                            <div className="flex-1">
+                                <p>Email Address</p>
+                                <input type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm outline-none focus:border-primary-100" />
+                            </div>
                         </div>
-                        <div className="flex-1">
-                            <p>Email Address</p>
-                            <input type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm" />
-                        </div>
-                    </div>
-                    <div className="flex flex-col mt-8">
-                        <div className="flex items-center justify-start mb-2 space-x-3">
-                            <input type="checkbox" required/>
-                            <p className="text-[16px] font-secondary font-bold">
-                                Permission of listening rights
+                        <div className="flex flex-col mt-8">
+                            <div className="flex items-center justify-start mb-2 space-x-3">
+                                <input type="checkbox" required/>
+                                <p className="text-[16px] font-secondary font-bold">
+                                    Permission of listening rights
+                                </p>
+                            </div>
+                            <p className="text-[13px] font-secondary lg:max-w-[468px] max-w-none">
+                                Confirm that you are providing permissions to Musixverse to use your music for non-monetized streaming, both on Musixverse and any external apps and platforms which are directly associated with or part of the Musixverse platform.
+                            </p>
+                            <div className="flex items-center justify-start pt-2 mt-3 space-x-3">
+                                <input type="checkbox" required/>
+                                <p className="text-[16px] font-secondary font-bold">
+                                    Guidelines for submission
+                                </p>
+                            </div>
+                            <p className="text-[13px] font-secondary lg:max-w-[468px] max-w-none mt-2">
+                                Confirm that none of your submissions,both current and future contain any infringing or unauthorized copyrighted material.
                             </p>
                         </div>
-                        <p className="text-[13px] font-secondary lg:max-w-[468px] max-w-none">
-                            Confirm that you are providing permissions to Musixverse to use your music for non-monetized streaming, both on Musixverse and any external apps and platforms which are directly associated with or part of the Musixverse platform.
-                        </p>
-                        <div className="flex items-center justify-start pt-2 mt-3 space-x-3">
-                            <input type="checkbox" required/>
-                            <p className="text-[16px] font-secondary font-bold">
-                                Guidelines for submission
-                            </p>
-                        </div>
-                        <p className="text-[13px] font-secondary lg:max-w-[468px] max-w-none mt-2">
-                            Confirm that none of your submissions,both current and future contain any infringing or unauthorized copyrighted material.
-                        </p>
+                        <Button>Continue</Button>
                     </div>
-                    <Button>Continue</Button>
+                    {/* Step-2 */}
+                    <div className="min-w-[468px]">
+                        <p className="text-[16px] font-primary font-bold">Tell us about yourself</p>
+                        <textarea spellCheck="false" ref={bio} className="font-secondary text-base w-full p-1 resize-none h-[200px] mt-2 border-2 border-gray-500 rounded-md shadow-sm outline-none focus:border-primary-100" ></textarea>
+                        <p className="text-[16px] font-primary font-bold mt-4">Add your socials(Optional)</p>
+                        <div className="mt-2">
+                            <p className="text-[16px] font-secondary font-bold">Instagram</p>
+                            <input spellCheck="false" ref={instagram} type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm outline-none font-secondary focus:border-primary-100" />
+                            <p className=" text-[16px] font-secondary font-bold">Facebook</p>
+                            <input spellCheck="false" ref={facebook} type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm outline-none font-secondary focus:border-primary-100" />
+                            <p className=" text-[16px] font-secondary font-bold">Twitter</p>
+                            <input spellCheck="false" ref={twitter} type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm outline-none font-secondary focus:border-primary-100" />
+                            <p className=" text-[16px] font-secondary font-bold">Spotify</p>
+                            <input spellCheck="false" ref={spotify} type="text" className="w-full p-1 border-2 border-gray-500 rounded-md shadow-sm outline-none font-secondary focus:border-primary-100" />
+                        </div>
+                        {/* Re-design buttons */}
+                        <div className="flex space-x-3">
+                            <Button>Submit</Button>
+                            <Button>Skip</Button>
+                        </div>
+                    </div>
                 </RightSection>
             </div>
         </div>

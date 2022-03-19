aa60e7a0f4b9598dd61fae9e420f23e76fb0cf31
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-03-20 00:20:24 +0530

    Added dark theme to Artist section

--- src/components/Registration/SocialDetails.js
+++ src/components/Registration/SocialDetails.js
@@ -46,7 +46,7 @@ export default function SocialDetails({ error, setError, success, setSuccess }) 
 
     return (
         <div className={styles["register"]}>
-            <div className={styles["register__container"]}>
+            <div className={"dark:bg-dark-200 "+styles["register__container"]}>
                 {/* Left section */}
                 <div className="lg:max-w-[30vw] pb-6 lg:pb-0">
                     <p className="mt-20 text-5xl font-tertiary max-w-[468px]">ARTIST SIGN UP</p>

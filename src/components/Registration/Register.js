b2fbe9681c5babea7bbccd26adb427d2f8b2e415
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-03-19 23:38:53 +0530

    resolve conflict

--- src/components/Registration/Register.js
+++ src/components/Registration/Register.js
@@ -1,19 +1,24 @@
+import { useTheme } from "next-themes";
 import ArtistOrUser from "./ArtistRegUtils/ArtistOrUser";
 import styles from "../../../styles/Registration/Register.module.css";
 
-export default function Register() {
-    return (
-        <div className={styles["register"]}>
-            <div className={styles["register__container"]}>
+export default function Register(){
+    const { theme } = useTheme();
+
+    return(
+        <div className={styles['register']}>
+            <div className={styles['register__container']}>
                 {/* Left section */}
-                <div className={styles["register__container--left-section"]}>
-                    <p className={styles["container__left-section--intro-text"]}>LET&apos;S REVOLUTIONIZE THE MUSIC INDUSTRY TOGETHER!</p>
-                    <img className={styles["container__left-section--asset-image"]} src="/assets/registration/dark_MXV.svg" alt="music illustration" />
+                <div className={styles['register__container--left-section']}>
+                    <p className={styles['container__left-section--intro-text']}>
+                        LET&apos;S REVOLUTIONIZE THE MUSIC INDUSTRY TOGETHER!
+                    </p>
+                    <img className={styles['container__left-section--asset-image']} src={theme === "light" ? "/assets/registration/dark_MXV.svg" : "/assets/registration/white_MXV.svg"} alt="music illustration" />
                 </div>
 
                 {/* Right section */}
-                <div className={styles["register__container--right-section"]}>
-                    <ArtistOrUser />
+                <div className={styles['register__container--right-section']}>
+                    <ArtistOrUser/>
                 </div>
             </div>
         </div>

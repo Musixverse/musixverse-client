3bcc413cc99adf7bf213b9fd2467c12a937068fa
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-03-19 14:50:18 +0530

    Made the modals functional via refs

--- src/components/ArtistRegistration/ArtistRegistration.js
+++ src/components/ArtistRegistration/ArtistRegistration.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import RightSection from "./ArtistRegUtils/RightSection";
 // import LeftSection from "./ArtistRegUtils/LeftSection";
 import styles from "../../../styles/ArtistRegistration/Artist.module.css";
@@ -6,15 +6,31 @@ import Button from "./ArtistRegUtils/Button";
 import SelectAvatar from "./ArtistRegUtils/SelectAvatar";
 import Router from "next/router";
 import { useMoralis } from "react-moralis";
+import YourDetails from "./ArtistRegUtils/YourDetails";
 
 export default function ArtistRegistration() {
     const { setUserData } = useMoralis();
-    const [name, setName] = useState("");
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
+    // const [name, setName] = useState("");
+    // const [username, setUsername] = useState("");
+    // const [email, setEmail] = useState("");
+    
+    const nameRef = useRef(null);
+    const usernameRef = useRef(null);
+    const emailRef = useRef(null);
+    const bioRef = useRef(null);
+    const facebookRef = useRef(null);
+    const twitterRef = useRef(null);
+    const spotifyRef = useRef(null);
+    const instagramRef = useRef(null);
+    const ref = useRef({nameRef, usernameRef, emailRef, bioRef, facebookRef, twitterRef, spotifyRef, instagramRef});
 
     const handleArtistDetailsSave = async (e) => {
         e.preventDefault();
+        console.log("called");
+        const name = nameRef.current.value;
+        const username = usernameRef.current.value;
+        const email = emailRef.current.value;
+        console.log("Current saved: ",name, username, email, bioRef.current.value, instagramRef.current.value, twitterRef.current.value,facebookRef.current.value, spotifyRef.current.value)
 
         if (name !== "" && username !== "" && email !== "") {
             await setUserData({
@@ -55,7 +71,15 @@ export default function ArtistRegistration() {
                 {/* Right section*/}
                 <RightSection>
                     <form onSubmit={handleArtistDetailsSave}>
-                        <p className="text-5xl font-tertiary max-w-[468px] mb-10">YOUR DETAILS</p>
+                        <YourDetails ref={ref}/>
+                    </form>
+                </RightSection>
+            </div>
+        </div>
+    );
+}
+/**
+ <p className="text-5xl font-tertiary max-w-[468px] mb-10">YOUR DETAILS</p>
                         <SelectAvatar />
 
                         <p className="mt-8 text-[16px] font-secondary font-bold">Your Name*</p>
@@ -114,9 +138,4 @@ export default function ArtistRegistration() {
                             </p>
                         </div>
                         <Button>Continue</Button>
-                    </form>
-                </RightSection>
-            </div>
-        </div>
-    );
-}
+ */

5c98ef4765ec8e9265fd162ea65348a99e0be449
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-05-02 12:28:34 +0530

    Added styling to images in banner section

--- src/components/CFB/BannerCFB.js
+++ src/components/CFB/BannerCFB.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import styles from "../../../styles/CFB/BannerCFB.module.css";
-import girl_svg from "../../../public/assets/CFB/Girl_Banner.png";
-import boy_svg from "../../../public/assets/CFB/Boy_Banner.png";
+// import girl_svg from "../../../public/assets/CFB/Girl_Banner.png";
+// import boy_svg from "../../../public/assets/CFB/Boy_Banner.png";
 
 export default function BannerCFB(){
     return(
         <div className={styles['cfb-banner__container']}>
-            <Image src={girl_svg} width={500} height={500} alt="Girl" />
+            <img src="/assets/CFB/Girl_Banner.png" className={styles['cfb-banner__left-image']} width={500} height={500} alt="Girl" />
             <div className={styles['cfb-banner__subcontainer']}>
                 <p className="font-primary font-semibold text-center">Hello there!</p>
                 <p className="text-3xl max-w-lg font-primary font-semibold text-center">We invite you to be a part of our Insider Community!</p>
@@ -17,7 +17,7 @@ export default function BannerCFB(){
                 <button className="text-white text-xs bg-primary-200 hover:bg-primary-300 font-primary rounded-xl px-5 py-3 mt-6">
                 Join Discord Community</button>
             </div>
-            <Image src={boy_svg} width={500} height={500} alt="Boy" />
+            <img src="/assets/CFB/Boy_Banner.png" className={styles['cfb-banner__right-image']} width={500} height={500} alt="Boy" />
         </div>
     );
 }

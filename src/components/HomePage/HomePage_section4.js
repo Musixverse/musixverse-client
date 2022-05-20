9b5bc2845cfcce7bf49e2926426c74b24d5da4ba
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-05-20 16:05:22 +0530

    Updated Home Page

---
+++ src/components/HomePage/HomePage_section4.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function HomePage_section4(){
+    return(
+        <>
+            <div className={styles['HomePage_section4__container']}>
+                <div>
+                    <p>Join our community to get early access and exclusive privileges on Musixverse</p>
+                    <p>Everything that we are doing today is going to power the decentralized world of tomorrow.</p>
+                    <p> Establishing ownership of these assets will be key to what you can and cannot do in the metaverse.</p>
+                </div>
+                <div className={styles['section2__card--innercard']}>
+                    
+                </div>
+
+            </div>
+        </>
+
+    )
+}

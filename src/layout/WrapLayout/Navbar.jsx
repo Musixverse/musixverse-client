6a64d2c6748f5c582d1309dfc088e8a265f12d93
Pushpit <pushpit07@gmail.com>
2022-06-05 19:18:29 +0530

    Updated Navbar
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/layout/WrapLayout/Navbar.jsx
+++ src/layout/WrapLayout/Navbar.jsx
@@ -22,14 +22,12 @@ const Navbar = () => {
                     </a>
                 </Link>
 
-                <div className="hidden w-full ml-auto md:block md:w-auto" id="mobile-menu"></div>
-
-                <div className="hidden ml-auto md:block">
-                    <ul className="flex flex-col items-center md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                        <li className="hover:text-primary-200">
+                <div className="ml-auto block">
+                    <ul className="flex flex-row items-center md:space-x-8 md:mt-0 sm:text-sm font-medium text-xs">
+                        <li className="hover:text-primary-200 hidden md:block">
                             <Link
                                 href="/"
-                                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded hover:text-primary-100 md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                                className="py-2 pl-3 pr-4 text-white rounded hover:text-primary-100 md:bg-transparent md:p-0 dark:text-white"
                                 aria-current="page"
                             >
                                 Home
@@ -38,24 +36,16 @@ const Navbar = () => {
                         <li className="hover:text-primary-200">
                             <Link
                                 href="/cfh/cfb"
-                                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:text-primary-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:text-primary-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                             >
                                 Community Feedback Board
                             </Link>
                         </li>
-                        <li className="hover:text-primary-200">
-                            <Link
-                                href="/contact"
-                                className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:text-primary-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                            >
-                                Contact Us
-                            </Link>
-                        </li>
-                        <li>
+                        <li className="ml-2">
                             <button
                                 aria-label="Toggle Dark Mode"
                                 type="button"
-                                className="p-1 h-12 w-12 flex items-center justify-center rounded-full hover:bg-light-300 dark:hover:bg-zinc-800"
+                                className="h-12 w-12 flex items-center justify-center rounded-full hover:bg-light-300 dark:hover:bg-zinc-800"
                                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
                             >
                                 <svg

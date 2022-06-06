d99f87684afecb1ee2419fc6823ab88bc4d6d8b5
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-06-06 16:10:53 +0530

    Fixed homepage banner image

--- src/components/HomePage/HomePageBanner.js
+++ src/components/HomePage/HomePageBanner.js
@@ -4,7 +4,8 @@ import { useTheme } from "next-themes";
 export default function Banner() {
     const { theme } = useTheme();
     return (
-        <>
+        /* Outer div for bg blur */
+        <div className={styles["hero-section-container"]}>
             {/* For bg image */}
             <div className={styles["background-image-div"]}>
                 <img
@@ -13,28 +14,25 @@ export default function Banner() {
                     alt="background image"
                 ></img>
             </div>
-            {/* Outer div for bg blur */}
-            <div className={styles["hero-section-container"]}>
-                {/* Inner div for content */}
-                <div className={"text-white dark:text-white " + styles["hero-section-container__content-div"]}>
-                    <h1 className={styles["hero-section-container__content-div--heading"]}>
-                        Empowering Artists and Fans
-                        <br /> like never before
-                    </h1>
-                    <p className={styles["hero-section-container__content-div--description"]}>
-                        Musixverse will bring together artists and fans like never before, help them do great things together and empower each other in the
-                        process.
-                    </p>
-                    <a
-                        className="px-5 py-3 text-xs text-white bg-primary-200 hover:bg-primary-300 font-primary rounded-3xl"
-                        href="https://discord.gg/sCxZyJmW"
-                        target={"_blank"}
-                        rel="noopener noreferrer"
-                    >
-                        Join the Discord Community
-                    </a>
-                </div>
+            {/* Inner div for content */}
+            <div className={"text-white dark:text-white " + styles["hero-section-container__content-div"]}>
+                <h1 className={styles["hero-section-container__content-div--heading"]}>
+                    Empowering Artists and Fans
+                    <br /> like never before
+                </h1>
+                <p className={styles["hero-section-container__content-div--description"]}>
+                    Musixverse will bring together artists and fans like never before, help them do great things together and empower each other in the
+                    process.
+                </p>
+                <a
+                    className="px-5 py-3 text-xs text-white bg-primary-200 hover:bg-primary-300 font-primary rounded-3xl"
+                    href="https://discord.gg/sCxZyJmW"
+                    target={"_blank"}
+                    rel="noopener noreferrer"
+                >
+                    Join the Discord Community
+                </a>
             </div>
-        </>
+        </div>
     );
 }

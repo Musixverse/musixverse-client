904ee594df137746ccb6f38fbf1aecd68b42a977
Pushpit <pushpit07@gmail.com>
2022-06-11 01:01:05 +0530

    Added google analytics and changed strategy of script
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,8 +7,8 @@ function App({ Component, pageProps }) {
     return (
         <>
             {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} />
-            <Script id="google-analytics" strategy="lazyOnload">
+            <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} />
+            <Script id="google-analytics" strategy="afterInteractive">
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}

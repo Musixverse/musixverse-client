291105423aabf79a86b230cea8b8a2d7d4511395
Pushpit <pushpit07@gmail.com>
2022-06-11 00:37:57 +0530

    Added Google Analytics
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,18 @@ import Layout from "../components/WrapLayout/Layout";
 function App({ Component, pageProps }) {
     return (
         <>
+            {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} />
+            <Script id="google-analytics" strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
             <Script src="https://kit.fontawesome.com/0366dd7992.js" crossOrigin="anonymous"></Script>
             <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></Script>
 

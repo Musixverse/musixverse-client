d049470bf6142df607aa075de024b608a6b86dc8
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-06-19 10:19:46 +0530

    Added create-NFT home component

--- src/components/CreateNFT/not-an-artist.js
+++ src/components/CreateNFT/not-an-artist.js
@@ -4,7 +4,7 @@ export default function Notartist(){
     return(
         <div className="pt-8 min-h-screen flex flex-col justify-center items-center">
             <div className="p-6 rounded-2xl bg-white dark:bg-dark-100">
-                <Image src="/assets/createNFT/astronaut.png" alt="Not an artist" width={50} height={50} />
+                <Image src="/assets/create-nft/astronaut.png" alt="Not an artist" width={50} height={50} />
             </div>
             <p className="font-tertiary text-4xl sm:text-5xl mt-6">OOPS! something&apos;s not right</p>
             <p className="font-secondary text-sm sm:text-base text-center">You don&apos;t have an Artist account to create an NFT. you <br/> need to switch to Artist to mint songs.</p>

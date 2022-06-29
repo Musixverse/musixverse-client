b04319f0e881f88ed9a061ebc63fa7aa01f5d85f
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-06-29 16:32:45 +0530

    added logic to connect contact-us with moralis server

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,15 +1,101 @@
-import React, { useState, Fragment } from "react";
+import { useState, Fragment, useRef, useContext } from "react";
 import styles from "../../../styles/ContactUs/contactUs.module.css";
+import { useMoralis, useNewMoralisObject } from "react-moralis";
+import StatusContext from "../../../store/status-context";
 
 export default function ContactUs() {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
+	const [, , , setError] = useContext(StatusContext);
+	const { save: saveUserMessage } = useNewMoralisObject("UserMessage");
+
+	const nameRef = useRef(null);
+    const emailRef = useRef(null);
+	const messageRef = useRef(null);
 
 	const onFocus = (event) => {
 		if (event.target.autocomplete) {
 			event.target.autocomplete = "No";
 		}
 	};
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+		const message = messageRef.current.value;
+
+        // EMAIL CHECKS
+        const emailRegex = new RegExp(
+            /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+        );
+        if (!emailRegex.test(email)) {
+            setError({
+                title: "Invalid credentials!",
+                message: "Please enter a valid email",
+                showErrorBox: true,
+            });
+            emailRef.current.focus();
+            return;
+        }
+
+		// NAME CHECKS
+		const nameRegex = /^\w+$/;
+        if (name.length < 2) {
+            setError({
+                title: "Invalid credentials!",
+                message: "Please enter a valid name",
+                showErrorBox: true,
+            });
+            nameRef.current.focus();
+            return;
+        } else if (!nameRegex.test(name)) {
+            setError({
+                title: "Invalid credentials!",
+                message: "Name can only contain alphabets, numbers, and '_'",
+                showErrorBox: true,
+            });
+            nameRef.current.focus();
+            return;
+        }
+
+		// Message Check 
+        if (message.length < 2) {
+            setError({
+                title: "Invalid credentials!",
+                message: "Please enter a valid message",
+                showErrorBox: true,
+            });
+            messageRef.current.focus();
+            return;
+        }
+
+        if (name!="" && email!="" && message !== "") {
+            const userData = {
+                user: name,
+				email: email,
+                message: message
+            };
+            await saveUserMessage(userData, {
+                onSuccess: (obj) => {
+                    // Execute any logic that should take place after the object is saved.
+                },
+                onError: (error) => {
+                    // Execute any logic that should take place if the save fails.
+                    // error is a Moralis.Error with an error code and message.
+                    setError((prevState) => ({
+                        ...prevState,
+                        title: "Failed to save user information",
+                        message: JSON.stringify(error.message),
+                        showErrorBox: true,
+                    }));
+                    return;
+                },
+            });
+		}
+		return;
+	}
 
 	return (
 		<Fragment>
@@ -46,17 +132,17 @@ export default function ContactUs() {
 
 					<div className={styles["contact_form"]}>
 							<div className={"dark:backdrop-blur-2xl dark:backdrop-brightness-200 "+styles["contact_us_box"]}>
-								<form action="https://getform.io/f/91471bcd-4363-49f9-9762-bb91ed85745b" method="POST">
+								<form onSubmit={handleFormSubmit}>
 									<div className="flex flex-col md:flex-row md:space-x-8 xl:space-x-14">
 										<div className="w-full md:w-1/3">
 											<div className={styles["inputBox"]}>
 												<label className="dark:opacity-50">Name&nbsp; (Optional)</label>
-												<input className="dark:bg-[#1a1a1a] mt-1" type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} autoComplete="off" onFocus={onFocus} />
+												<input className="dark:bg-[#1a1a1a] mt-1" type="text" ref={nameRef} name="name" value={name} onChange={(e) => setName(e.target.value)} autoComplete="off" onFocus={onFocus} />
 											</div>
 
 											<div className={"mt-4 "+styles["inputBox"]}>
 												<label className="dark:opacity-50">Email Address&nbsp; (Optional)</label>
-												<input className="dark:bg-[#1a1a1a] mt-1" type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="off" onFocus={onFocus} />
+												<input className="dark:bg-[#1a1a1a] mt-1" type="email" ref={emailRef} name="email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="off" onFocus={onFocus} />
 											</div>
 
 											<div className="mt-5">
@@ -67,7 +153,7 @@ export default function ContactUs() {
 										<div className="w-full mt-5 md:w-2/3 md:mt-0">
 											<div>
 												<label className="text-sm font-primary dark:opacity-50">Message</label>
-												<textarea className={"dark:bg-[#1a1a1a] mt-1 "+styles["textarea_contact"]} name="message" rows="8" required></textarea>
+												<textarea className={"dark:bg-[#1a1a1a] mt-1 "+styles["textarea_contact"]} ref={messageRef} name="message" rows="8" required></textarea>
 											</div>
 
 											<button className={styles["send_message_btn"]}>

f64d17c15ded17a24f5e4442eed89fa2ad09d02b
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-07-02 19:20:21 +0530

    updated page with asked changes

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -3,7 +3,7 @@ import styles from "../../../styles/PageNotFound/PageNotFound.module.css";
 function PageNotFound() {
 	return (
 		<div className={styles["page_not_found_container"]}>
-			<div className={"dark:backdrop-blur-2xl dark:backdrop-brightness-200 "+styles["wrapper"]}>
+			<div className={"dark:bg-search-200 "+styles["wrapper"]}>
 				<div className={styles["page-wrapper"]}>
 					<div className={styles["needle-wrapper"]}>
 						<div className={styles["base"]}></div>

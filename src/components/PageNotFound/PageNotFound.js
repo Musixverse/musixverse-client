abd67234986a706525ddfd657825c6f76c74876b
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-07-02 17:34:58 +0530

    final changes to 404-page

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,38 +1,35 @@
-import React, { Fragment } from "react";
 import styles from "../../../styles/PageNotFound/PageNotFound.module.css";
 
 function PageNotFound() {
 	return (
-		<Fragment>
-			<div className="page_not_found_container">
-				<div className="wrapper glass_effect glass_effect_border">
-					<div className="page-wrapper">
-						<div className="needle-wrapper">
-							<div className="base"></div>
-							<div className="lines">
-								<div className="vertical"></div>
-								<div className="horizontal"></div>
-							</div>
+		<div className={styles["page_not_found_container"]}>
+			<div className={"dark:bg-dark-200 dark:backdrop-blur-2xl dark:backdrop-brightness-200 "+styles["wrapper"]}>
+				<div className={styles["page-wrapper"]}>
+					<div className={styles["needle-wrapper"]}>
+						<div className={styles["base"]}></div>
+						<div className={styles["lines"]}>
+							<div className={styles["vertical"]}></div>
+							<div className={styles["horizontal"]}></div>
 						</div>
-						<div className="disk-wrapper">
-							<div className="light-left"></div>
-							<div className="light-right"></div>
-							<div className="disk">
-								<div className="half-circle-top"></div>
-								<div className="half-circle-bottom"></div>
-								<div className="separator"></div>
-								<div className="inner-circle">
-									<span>4</span>
-									<div className="dot"></div>
-									<span>4</span>
-								</div>
+					</div>
+					<div className={styles["disk-wrapper"]}>
+						<div className={styles["light-left"]}></div>
+						<div className={styles["light-right"]}></div>
+						<div className={styles["disk"]}>
+							<div className={styles["half-circle-top"]}></div>
+							<div className={styles["half-circle-bottom"]}></div>
+							<div className={styles["separator"]}></div>
+							<div className={styles["inner-circle"]}>
+								<span>4</span>
+								<div className={styles["dot"]}></div>
+								<span>4</span>
 							</div>
 						</div>
 					</div>
-					<div className="error">Uh oh! Looks like we&apos;ve skipped a beat </div>
 				</div>
+				<div className={styles["error"]}>Uh oh! Looks like we&apos;ve skipped a beat </div>
 			</div>
-		</Fragment>
+		</div>
 	);
 }
 

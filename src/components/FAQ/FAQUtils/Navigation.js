0cfff3fc68e0ef1cc4f00d74e337fae44ef07185
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-07-03 20:11:19 +0530

    Implemented regex and FAQ darkmode

--- src/components/FAQ/FAQUtils/Navigation.js
+++ src/components/FAQ/FAQUtils/Navigation.js
@@ -4,12 +4,12 @@ export default function Navigation({currentSelection, setCurrentSelection, categ
             <li 
                 key={idx}
                 onClick={()=>setCurrentSelection(idx)}
-                className={(currentSelection === idx? "border-l-4 border-primary-200 text-primary-200 ":"text-gray-800 ")+"sm:text-base text-sm px-3 py-2 cursor-pointer hover:text-primary-200"}
+                className={(currentSelection === idx? "border-l-4 border-primary-200 text-primary-200 ":"text-gray-800 dark:text-light-200 ")+"sm:text-base text-sm px-3 py-2 cursor-pointer dark:hover:text-primary-200 hover:text-primary-200"}
             >{elem}</li>
         );
     })
     return(
-        <ul className="self-start w-full p-8 lg:max-w-xs rounded-xl font-secondary bg-light-300">
+        <ul className="self-start w-full p-8 lg:max-w-xs rounded-xl font-secondary bg-light-100 dark:bg-dark-100">
             {navItems}
         </ul>
     );

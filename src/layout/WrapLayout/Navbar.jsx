f3d712163a48fb042733051c6d88c780b226b758
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-07-15 23:18:44 +0530

    Added notification button on Navbar
    Signed-off-by: Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>

--- src/layout/WrapLayout/Navbar.jsx
+++ src/layout/WrapLayout/Navbar.jsx
@@ -88,11 +88,11 @@ const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
                     <ul className="flex flex-row items-center text-sm font-medium md:space-x-8 md:mt-0 sm:text-sm">
                         {/* Search bar */}
                         <li>
-                            <div className="flex items-center justify-center text-xs rounded text-dark-200 dark:text-search-300">
+                            <div className="flex items-center justify-center text-xs rounded text-dark-200 dark:text-white">
                                 <div className="flex overflow-hidden border rounded-full dark:border-dark-100">
                                     <button className="flex items-center justify-center px-4 border-l border-light-300 dark:border-dark-100 bg-search-100 dark:bg-search-200">
                                         <svg
-                                            className="w-4 h-4 dark:text-search-300"
+                                            className="w-4 h-4 dark:text-white"
                                             fill="currentColor"
                                             xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 24 24"
@@ -109,40 +109,20 @@ const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
                             </div>
                         </li>
                         
-                        {/* Toggle theme button */}
-                        <li className="ml-2">
+                        {/* Notification button */}
+                        <li>
                             <button
-                                aria-label="Toggle Dark Mode"
-                                type="button"
-                                className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-[#bdbdbd] dark:hover:bg-zinc-800"
-                                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="currentColor"
-                                    stroke="currentColor"
-                                    className="w-6 h-6 text-gray-800 dark:text-gray-200"
-                                >
-                                    {theme === "dark" ? (
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                                        />
-                                    ) : (
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                                        />
-                                    )}
-                                </svg>
+                                className="flex items-center relative justify-center text-base p-2 rounded-full bg-search-100 dark:bg-search-200 text-dark-200 dark:text-white">
+                                {/* Notification icon - when notification is ON */}
+                                <i className="fa fa-bell"></i>
+                                {/* Notification icon - when notification is OFF */}
+                                {/* <i className="fa fa-bell-slash"></i> */}
+
+                                {/* Notification badge */}
+                                <div className="bg-red-600 rounded-full w-1.5 h-1.5 absolute top-1/3 right-1/4" />
                             </button>
                         </li>
-
+                        
                         {/* Dropdowm Menu */}
                         <li>
                             <ul className="relative group dropdown">
@@ -206,6 +186,39 @@ const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
                                             </Link>
                                         </li>
                                     )}
+                                    {/* Toggle theme button */}
+                                    <li>
+                                        <button
+                                            aria-label="Toggle Dark Mode"
+                                            type="button"
+                                            className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-[#bdbdbd] dark:hover:bg-zinc-800"
+                                            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                fill="currentColor"
+                                                stroke="currentColor"
+                                                className="w-6 h-6 text-gray-800 dark:text-gray-200"
+                                            >
+                                                {theme === "dark" ? (
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth={2}
+                                                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                                                    />
+                                                ) : (
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth={2}
+                                                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                                                    />
+                                                )}
+                                            </svg>
+                                        </button>
+                                    </li>
                                 </ul>
                             </ul>
                         </li>

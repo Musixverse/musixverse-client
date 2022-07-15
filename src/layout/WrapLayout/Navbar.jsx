08a0cdfecbe19fbc2147a376f81767cd3dbb9b04
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-07-16 00:47:52 +0530

    Added Dropdown menu in navbar
    Signed-off-by: Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>

--- src/layout/WrapLayout/Navbar.jsx
+++ src/layout/WrapLayout/Navbar.jsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { useMoralis } from "react-moralis";
 import logoBlack from "../../../public/logo-black.svg";
 import logoWhite from "../../../public/logo-white.svg";
+import navIcon1 from "../../../public/nav-icon-1.png";
+import navIcon2 from "../../../public/nav-icon-2.png";
 
 const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
     const { theme, setTheme } = useTheme();
@@ -38,9 +40,9 @@ const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
                 <Link href="/">
                     <a href="#" className="flex">
                         {theme === "light" ? (
-                            <Image src={logoBlack} alt="MXV Logo" width="104" height="20" />
+                            <Image src={logoBlack} alt="MXV Logo" width="100" height="20" />
                         ) : (
-                            <Image src={logoWhite} alt="MXV Logo" width="104" height="20" />
+                            <Image src={logoWhite} alt="MXV Logo" width="100" height="20" />
                         )}
                     </a>
                 </Link>
@@ -134,19 +136,26 @@ const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
                                     data-bs-toggle="dropdown"
                                     aria-expanded="false"
                                 >
-                                    {theme === "dark" ? (
-                                        <Image src="https://img.icons8.com/nolan/512/apple-settings.png" width="35" height="35" />
-                                    ) : (
-                                        <Image src="https://img.icons8.com/ios/500/000000/apple-settings.png" width="35" height="35" />
-                                    )}
+                                 {isAuthenticated && user ? (
+                                    <div className="flex px-4 py-2 text-sm items-center justify-center rounded-full bg-search-100 dark:bg-search-200">
+                                        <span className="mr-4">Mathew S.</span>
+                                        <Image src={navIcon1} alt="avatar" width="24" height="24" />
+                                    </div>
+                                 ):(
+                                    <div className="flex px-10 py-2 font-semibold text-base items-center justify-center rounded-full bg-search-100 dark:bg-search-200">
+                                        Sign in 
+                                    </div>    
+                                 )}   
                                 </a>
                                 <ul
-                                    className="absolute right-0 left-auto z-10 hidden float-left py-2 m-0 mt-1 text-left list-none border-none rounded-lg shadow-lg dropdown-menu min-w-[150px] bg-light-100 bg-clip-padding group-hover:block"
+                                    className="absolute right-0 left-auto z-10 hidden text-base float-left py-2 m-0 mt-4 text-left list-none border-none rounded-xl shadow-lg dropdown-menu min-w-[250px] 
+                                    backdrop-blur-[40px] backdrop-brightness-200 bg-[rgba(255,255,255,0.8)] dark:bg-[rgba(19,19,19,0.4)] dark:backdrop-blur-[24px] dark:backdrop-brightness-105
+                                    bg-clip-padding group-hover:block"
                                     aria-labelledby="dropdownMenuButton2"
                                 >
                                     {isAuthenticated && user ? (
                                         <button
-                                            className="w-full px-4 py-2 font-medium text-gray-700 transition-all bg-transparent border-b cursor-pointer border-light-300 hover:bg-primary-100 hover:text-light-100"
+                                            className="w-full px-4 py-2 font-medium transition-all bg-transparent border-b cursor-pointer border-dark-200 dark:border-light-300 hover:bg-primary-100 hover:text-light-100"
                                             onClick={handleLogout}
                                         >
                                             <Link
@@ -157,7 +166,7 @@ const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
                                             </Link>
                                         </button>
                                     ) : (
-                                        <div className="px-4 py-2 text-gray-700 bg-transparent border-b border-light-300 hover:bg-primary-100 hover:text-light-100">
+                                        <div className="px-4 py-2 bg-transparent border-b border-dark-200 dark:border-light-300 hover:bg-primary-100 hover:text-light-100">
                                             <a
                                                 className="block w-full text-sm dropdown-item whitespace-nowrap hover:bg-primary-100 active:bg-primary-100"
                                                 href="#"
@@ -171,7 +180,7 @@ const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
                                     {user && isAuthenticated && (
                                         <li>
                                             <Link href={`/profile/${user.attributes.username}`}>
-                                                <div className="block w-full px-4 py-2 text-sm text-gray-700 bg-transparent cursor-pointer dropdown-item whitespace-nowrap hover:bg-gray-100">
+                                                <div className="block w-full px-4 py-2 bg-transparent cursor-pointer dropdown-item whitespace-nowrap hover:bg-gray-100 dark:hover:bg-dark-200">
                                                     Profile
                                                 </div>
                                             </Link>
@@ -180,44 +189,94 @@ const Navbar = ({ authModalOpen, setAuthModalOpen }) => {
                                     {user && isAuthenticated && (
                                         <li>
                                             <Link href="/settings/profile-settings">
-                                                <div className="block w-full px-4 py-2 text-sm text-gray-700 bg-transparent cursor-pointer dropdown-item whitespace-nowrap hover:bg-gray-100">
-                                                    Settings
+                                                <div className="block w-full px-4 py-2 bg-transparent cursor-pointer dropdown-item whitespace-nowrap hover:bg-gray-100 dark:hover:bg-dark-200">
+                                                    Dashboard
                                                 </div>
                                             </Link>
                                         </li>
                                     )}
+                                    {user && isAuthenticated && (
+                                        <li>
+                                            <Link href="/settings/notifications-settings">
+                                                <div className="block w-full px-4 py-2 bg-transparent cursor-pointer dropdown-item whitespace-nowrap hover:bg-gray-100 dark:hover:bg-dark-200">
+                                                    Notification Settings
+                                                </div>
+                                            </Link>
+                                        </li>
+                                    )}
+                                    {user && isAuthenticated && (
+                                        <li>
+                                            <Link href="/settings/account-help">
+                                                <div className="block w-full px-4 py-2 bg-transparent cursor-pointer dropdown-item whitespace-nowrap hover:bg-gray-100 dark:hover:bg-dark-200">
+                                                    Account Help
+                                                </div>
+                                            </Link>
+                                        </li>
+                                    )}
+                                    {user && isAuthenticated && (
+                                        <li>
+                                            <Link href="/contact-us">
+                                                <div className="block w-full px-4 py-2 bg-transparent cursor-pointer dropdown-item whitespace-nowrap hover:bg-gray-100 dark:hover:bg-dark-200">
+                                                    Contact Us
+                                                </div>
+                                            </Link>
+                                        </li>
+                                    )}
+
+
                                     {/* Toggle theme button */}
                                     <li>
-                                        <button
-                                            aria-label="Toggle Dark Mode"
-                                            type="button"
-                                            className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-[#bdbdbd] dark:hover:bg-zinc-800"
-                                            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 24 24"
-                                                fill="currentColor"
-                                                stroke="currentColor"
-                                                className="w-6 h-6 text-gray-800 dark:text-gray-200"
-                                            >
-                                                {theme === "dark" ? (
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        strokeWidth={2}
-                                                        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                                                    />
-                                                ) : (
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        strokeWidth={2}
-                                                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                                                    />
-                                                )}
-                                            </svg>
-                                        </button>
+                                        {theme === "dark" ? (
+                                            <div className="flex justify-between px-4 items-center w-full rounded-full bg-search-100 dark:bg-search-200">
+                                                <p>Switch to Light mode</p>
+                                                <button
+                                                    aria-label="Toggle Dark Mode"
+                                                    type="button"
+                                                    className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-[#bdbdbd] dark:hover:bg-zinc-800"
+                                                    onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                                                >
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 24 24"
+                                                        fill="currentColor"
+                                                        stroke="currentColor"
+                                                        className="w-6 h-6 text-gray-800 dark:text-gray-200"
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            strokeWidth={2}
+                                                            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        ):(
+                                            <div className="flex justify-between px-4 items-center w-full rounded-full bg-search-100 dark:bg-search-200">
+                                                <p>Switch to Dark mode</p>
+                                                <button
+                                                    aria-label="Toggle Dark Mode"
+                                                    type="button"
+                                                    className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-[#bdbdbd] dark:hover:bg-zinc-800"
+                                                    onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+                                                >
+                                                    <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24"
+                                                    fill="currentColor"
+                                                    stroke="currentColor"
+                                                    className="w-6 h-6 text-gray-800 dark:text-gray-200"
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            strokeWidth={2}
+                                                            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                                                        />   
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        )}     
                                     </li>
                                 </ul>
                             </ul>

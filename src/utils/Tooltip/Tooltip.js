69c8f154772c206c3737c34431f262473963205f
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-07-16 23:10:43 +0530

    remove bug from Tooltips
    Signed-off-by: Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>

--- src/utils/Tooltip/Tooltip.js
+++ src/utils/Tooltip/Tooltip.js
@@ -1,4 +1,3 @@
-import styles from "../../../styles/Tooltip/Tooltip.module.css";
 
 export default function Tooltips(props) {
 	const labelText = props.labelText;
@@ -6,7 +5,7 @@ export default function Tooltips(props) {
 	const tooltipLocation = props.tooltipLocation;
 
 	return (
-		<button type="button" className={"btn btn-secondary "+styles["tooltip-button"]} data-tooltip={message} data-tooltip-location={tooltipLocation ? tooltipLocation : "top"}>
+		<button type="button" className={"btn btn-secondary tooltip-button"} data-tooltip={message} data-tooltip-location={tooltipLocation ? tooltipLocation : "top"}>
 			{labelText}
 		</button>
 	);

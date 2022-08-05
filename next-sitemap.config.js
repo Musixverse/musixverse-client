30b932a83874e2c04281556df2faf85ee7749f37
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-08-05 09:37:27 +0530

    added sitemap for static pages
    Signed-off-by: Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://musixverse.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
+
+
+// TODO: add Additionals sitemap link to this component

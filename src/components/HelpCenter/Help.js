6c39ba7328931d0d55d1d58389941a14b543b280
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-09-07 01:36:01 +0530

    created component files for Help center  page

--- src/components/HelpCenter/Help.js
+++ src/components/HelpCenter/Help.js
@@ -9,19 +9,6 @@ export default function Help() {
 					<div>
 						<div className={styles["help_us_heading"]}>Help Center</div>
 					</div>
-
-					{/* <div className="mt-4">
-                        <div className={styles["help_notice"]}>
-                            Please note that{" "}
-                            <a href="http://musixverse.com" className="text-primary-100">
-                                musixverse.com
-                            </a>{" "}
-                            aggregates all tracks that users put up as NFTs and is not partnering with any of the artists displayed on the website. If you want
-                            to contact one of the artists listed here, you must contact them directly through other social media platforms. We are not
-                            responsible if someone else puts up an artists&apos; track as an NFT on our platform and cannot assist you regarding the same.
-                        </div>
-                    </div> */}
-
 					<div className={styles["email_us"]}>
 						<div>
 							For any kind of help, please head over to the support channel on discord and ask your question there.

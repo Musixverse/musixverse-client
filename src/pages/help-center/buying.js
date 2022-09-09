5a0abbff13d0dcc358dbf7b089306ff97039b803
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-09-09 19:19:43 +0530

    styling fixes in help center pages

--- src/pages/help-center/buying.js
+++ src/pages/help-center/buying.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { meta_description } from "../../constants";
-import Help from "../../components/HelpCenter/Help";
+import Buying from "../../components/HelpCenter/Buy";
 
 const BuyingNFT = ({}) => {
 	return (
@@ -13,7 +13,7 @@ const BuyingNFT = ({}) => {
 
 			<div className="flex flex-col items-center justify-center w-full bg-light-200 dark:bg-dark-200">
 				<div className="w-full max-w-[1920px] px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-36">
-					<Help />
+					<Buying />
 				</div>
 			</div>
 		</>

ab331acfd6a102ab5163564f56b7176794525ebe
Pushpit <pushpit07@gmail.com>
2022-09-10 21:37:07 +0530

    Updated moralis server and fixed ipfs issue @Sparsh
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/utils/image-crop/uploadFileToIPFS.js
+++ src/utils/image-crop/uploadFileToIPFS.js
@@ -3,10 +3,10 @@
  */
 
 export default async function uploadFileToIPFS(Moralis, fileToUpload) {
-    if (fileToUpload === null) return null;
-    const file = new Moralis.File("file", fileToUpload);
-    console.log("we have file");
-    await file.saveIPFS();
-    console.log("file is saved on ipfs");
-    return file.ipfs();
+	if (fileToUpload === null) return null;
+	const file = new Moralis.File("file", fileToUpload);
+	console.log("we have file", file);
+	await file.saveIPFS();
+	console.log("file is saved on ipfs");
+	return file.ipfs();
 }

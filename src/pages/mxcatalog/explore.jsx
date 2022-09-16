7af646a32c489b9dfca033ea424d260b8b996703
Pushpit <pushpit07@gmail.com>
2022-09-16 15:05:31 +0530

    Added getStaticProps on mxcatalog
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/pages/mxcatalog/explore.jsx
+++ src/pages/mxcatalog/explore.jsx
@@ -2,6 +2,21 @@ import Head from "next/head";
 import { meta_description } from "../../constants";
 import Marketplace from "../../components/MxCatalog/Utils/Marketplace";
 
+export async function getStaticProps(context) {
+	// Passing data to the Page using props
+	return {
+		props: {},
+		revalidate: 10,
+	};
+}
+
+export function getStaticPaths() {
+	return {
+		paths: [],
+		fallback: "blocking",
+	};
+}
+
 export default function Explore() {
 	return (
 		<>

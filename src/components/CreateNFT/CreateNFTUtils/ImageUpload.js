b0d0261fb274efbd5003994be487460b80d77798
Pushpit <pushpit07@gmail.com>
2022-09-17 23:58:22 +0530

    Updated with correct production contract address
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/components/CreateNFT/CreateNFTUtils/ImageUpload.js
+++ src/components/CreateNFT/CreateNFTUtils/ImageUpload.js
@@ -66,7 +66,9 @@ export default function ImageUpload({ coverArtUrl, setCoverArtUrl, setCoverArtMi
 					{coverArtUrl !== null ? (
 						<p className="text-sm text-primary-200">Image Uploaded</p>
 					) : (
-						<p className="text-xs">Any Image file | Max file size: 1GB <br /> Recommended size: 640 x 640 px.</p>
+						<p className="text-xs">
+							Any Image file | Max file size: 1GB <br /> Recommended size: 640 x 640 px.
+						</p>
 					)}
 				</div>
 			</label>

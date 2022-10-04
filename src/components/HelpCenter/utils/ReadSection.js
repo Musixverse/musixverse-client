00e240cc0d332a14f7e5f452e6f384d666df64c3
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-10-04 16:20:31 +0530

    Added Blog page for How to create NFT

--- src/components/HelpCenter/utils/ReadSection.js
+++ src/components/HelpCenter/utils/ReadSection.js
@@ -6,7 +6,7 @@ export default function ReadSection(){
             <h1 className="text-center text-3xl">Recommended Reads</h1>
             <div className="grid grid-cols-3 gap-5 mt-10">
                 <div className="bg-white text-lg font-primary p-12 text-center rounded-lg cursor-pointer shadow hover:shadow-xl duration-300">
-                    <Link href="/blog/how-to-open-account">How do I open a Musixverse account?</Link>
+                    <Link href="/help-center/blog/how-to-open-account">How do I open a Musixverse account?</Link>
                 </div>
                 <div className="bg-white text-lg font-primary p-12 text-center rounded-lg cursor-pointer shadow hover:shadow-xl duration-300">
                     <Link href="/blog/crypto-wallets-mxv">What crypto wallets can I use with Musixverse?</Link>
@@ -15,7 +15,7 @@ export default function ReadSection(){
                     <Link href="/blog/how-to-sell">How do I sell an NFT?</Link>
                 </div>
                 <div className="bg-white text-lg font-primary p-12 text-center rounded-lg cursor-pointer shadow hover:shadow-xl duration-300">
-                    <Link href="/blog/how-to-create-nft">How do I create an NFT?</Link>
+                    <Link href="/help-center/blog/how-to-create-nft">How do I create an NFT?</Link>
                 </div>
                 <div className="bg-white text-lg font-primary p-12 text-center rounded-lg cursor-pointer shadow hover:shadow-xl duration-300">
                     <Link href="/blog/how-to-transact">How can I buy MATIC to transact on Musixverse?</Link>

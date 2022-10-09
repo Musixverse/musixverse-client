4bdb6d03e7c64752d785a758f121fd0177442425
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-10-09 17:01:52 +0530

    Added new TopArtist section on Homepage

--- src/components/NewHomePage/TopArtist.js
+++ src/components/NewHomePage/TopArtist.js
@@ -9,28 +9,34 @@ import Sommaiya from "../../../public/assets/new-homepage/sommaiya.png";
 
 export default function TopArtist(){
     return(
-        <div>
-            <h1 className="font-primary text-2xl text-center">Top Artist on M<span className="text-primary-100">x</span> Catalog</h1>
-            <div className="w-full mt-10">
-                <Image src={CoverImage} alt="Cover art for Top Artist" layout="responsive" priority />
-                <div className="flex gap-4">
-                    <span className="backdrop-blur-md font-primary text-white rounded-xl">
-                        <Link href="/mx-catalog" passHref={true}>
-                            <span>View all</span>
-                        </Link>
-                    </span>
-                    <span className="backdrop-blur-md font-primary text-white rounded-full">
-                        <Link href="/help-center" passHref={true}>
-                            <span><i className="fa-solid fa-circle-info"></i></span>
-                        </Link>
-                    </span>
+        <div className="relative pb-48">
+            <h1 className="font-primary text-3xl text-center">Top Artist on M<span className="text-primary-100">x</span> Catalog</h1>
+            <div className="w-full mt-10 relative">
+                <div className="text-sm z-10 absolute right-36 mt-10">
+                    <div className="space-x-4">
+                        <span className="p-2 font-primary cursor-pointer rounded-3xl">
+                            <Link href="/mx-catalog" passHref={true}>
+                                <span>View all</span>
+                            </Link>
+                        </span>
+                        <span className="p-2 font-primary cursor-pointer rounded-full">
+                            <Link href="/help-center" passHref={true}>
+                                <span><i className="fa-solid fa-circle-info"></i></span>
+                            </Link>
+                        </span>
+                    </div>
                 </div>
+                <Image src={CoverImage} alt="Cover art for Top Artist" layout="responsive" priority />
             </div>
-            <div className="flex gap-x-4">
-                <ArtistCard name="Sarthak Kalyani" imageURL={Sarthak_Kalyani} description="The Singer who wowed AR Rahman" />
-                <ArtistCard name="Jatayu" imageURL={Jatayu} description="The Jazz-Rock Quartet introducing Indian tunes to the world" />
-                <ArtistCard name="Submarine in Space" imageURL={Submarine_in_space} description="The Signer touching all the right cords" />
-                <ArtistCard name="Sommaiya" imageURL={Sommaiya} description="The artist taking Indian Hip-hop Global" />
+            <div className="w-full flex absolute">
+                <div className="inline-block w-4 h-20 bg-primary-100 rounded-2xl relative left-[122px] -top-4"></div>
+                <div className="w-full flex justify-center gap-x-8 relative bottom-40">
+                    <ArtistCard name="Sarthak Kalyani" imageURL={Sarthak_Kalyani} description="The Singer who wowed AR Rahman" />
+                    <ArtistCard name="Jatayu" imageURL={Jatayu} description="The Jazz-Rock Quartet introducing Indian tunes to the world" />
+                    <ArtistCard name="Submarine in Space" imageURL={Submarine_in_space} description="The Signer touching all the right cords" />
+                    <ArtistCard name="Sommaiya" imageURL={Sommaiya} description="The artist taking Indian Hip-hop Global" />
+                </div>
+                <div className="inline-block w-4 h-20 bg-primary-100 rounded-2xl relative right-[122px] -top-4"></div>
             </div>
         </div>
     );
--- src/components/NewHomePage/utils/ArtistCard.js
+++ src/components/NewHomePage/utils/ArtistCard.js
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
-import CoverArt from "../../../../public/assets/new-homepage/sarthak_kalyani.png";
 
-export default function ArtistCard(imageURL,name,description){
+export default function ArtistCard({imageURL,name,description}){
     return(
-        <div className="flex flex-col justify-between p-8 rounded-lg w-1/4 h-96">
-            <span className="flex justify-end text-white">
-                <Link href="/profile/sarthak" passHref={true}>
-                    <i className="fa-solid fa-arrow-right"></i>
-                </Link>
-            </span>
-            <Image src={imageURL ? imageURL : CoverArt} alt="Top artist cover art" layout="responsive" priority></Image>
-            <h1 className="font-tertiary uppercase">{name}</h1>
-            <p className="font-primary mt-2">{description}</p>
+        <div className="relative z-10">
+            <div className="flex flex-col justify-end rounded-lg w-56 h-80">
+                <div className="absolute flex w-full h-full">
+                    <Image src={imageURL} alt="Top artist cover art" objectFit="fill" priority></Image>
+                </div>
+                <span className="flex justify-end text-white cursor-pointer z-10 absolute right-4 top-4">
+                    <Link href="/profile/sarthak" passHref={true}>
+                        <i className="fa-solid fa-arrow-right"></i>
+                    </Link>
+                </span>
+                <div className="z-10 p-8 align-bottom">
+                    <h1 className="font-tertiary text-2xl uppercase">{name}</h1>
+                    <p className="font-primary text-sm mt-2">{description}</p>
+                </div>
+            </div>
         </div>
     );
 }

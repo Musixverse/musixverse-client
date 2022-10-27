e18e6096b65e7611e9847f11b4ece652cc646bba
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-10-28 00:01:19 +0530

    Added meta data of Top Artost

---
+++ src/components/NewHomePage/utils/TopArtistData.js
@@ -0,0 +1,34 @@
+
+
+export const TopArtist = [
+    {
+        name:"Sarthak Kalyani",
+        instagramProfileURL:"https://www.instagram.com/sarthak.kalyani",
+        imageURL: require("../../../../public/assets/new-homepage/sarthak_kalyani.png"),
+        description:"The singer who wowed AR Rahman"
+    },
+    {
+        name:"Jatayu",
+        instagramProfileURL:"https://www.instagram.com/jatayusounds",
+        imageURL:require("../../../../public/assets/new-homepage/jatayu.png"),
+        description:"A Jazz Rock Fusion Band introducing the world to Indian tunes"
+    },
+    {
+        name:"Submarine in Space",
+        instagramProfileURL:"https://www.instagram.com/submarine_in_space",
+        imageURL:require("../../../../public/assets/new-homepage/submarine_in_space.png"),
+        description:"The Critically Acclaimed IInstrumental Jazz-Rock Band"
+    },
+    {
+        name:"Sommaiya",
+        instagramProfileURL:"https://www.instagram.com/sommaiya_angrish",
+        imageURL:require("../../../../public/assets/new-homepage/sommaiya.png"),
+        description:"A Genreless Musician and Filmmaker leaving his mark from New Delhi to New York"
+    },
+    {
+        name:"Harry Arora",
+        instagramProfileURL:"https://www.instagram.com/harrythisside",
+        imageURL:require("../../../../public/assets/new-homepage/harry.png"),
+        description:"The singer touching all the right chords"
+    },
+]

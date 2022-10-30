063464f05d26d7290d8f085ddbc0efc00d75926d
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-10-30 23:13:03 +0530

    added responsiveness and custom styling

--- src/components/NewHomePage/utils/SwiperArtist.js
+++ src/components/NewHomePage/utils/SwiperArtist.js
@@ -6,54 +6,62 @@ import { Navigation, Mousewheel, Keyboard } from "swiper";
 import ArtistCard from "./ArtistCard";
 import { TopArtist } from "./TopArtistData";
 
-export default function SwiperArtist(){
+export default function SwiperArtist({setCurrBg}){
+
+    const onHover = function(item) {
+        setCurrBg(item.bgURL);
+    }
+
     return(
-        <Swiper
-            breakpoints={
-                {
-                    320: {
-                        slidesPerView: 1,
-                        spaceBetween: 10,
-                    },
-                    640: {
-                        slidesPerView: 2,
-                        spaceBetween: 20,
-                    },
-                    768: {
-                        slidesPerView: 3,
-                        spaceBetween: 20,
-                    },
-                    1024: {
-                        slidesPerView: 4,
-                        spaceBetween: 25,
-                    },
+        <div className="swiper-container">
+            <Swiper
+                breakpoints={
+                    {
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 10,
+                        },
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                        },
+                        1280:{
+                            slidesPerView: 4,
+                            spaceBetween: 40,
+                        }
+                    }
                 }
-            }
-            // slidesPerView={4}
-            // spaceBetween={25}
-            autoHeight={false}
-            autowidth={false}
-            // centeredSlides={true}
-            enabled={true}
-            cssMode={true}
-            navigation={true}
-            mousewheel={true}
-            keyboard={true}
-            modules={[Navigation, Mousewheel, Keyboard]}
-            className="mx-10 absolute bottom-32"
-        >
-            {TopArtist.map((item, index) => {
-                return (
-                    <SwiperSlide key={index}>
-                        <ArtistCard
-                            name={item.name}
-                            instagramProfileURL={item.instagramProfileURL}
-                            imageURL={item.imageURL}
-                            description={item.description}
-                        />
-                    </SwiperSlide>
-                );
-            })}
-        </Swiper>
+                autoHeight={false}
+                autowidth={false}
+                enabled={true}
+                cssMode={true}
+                navigation={true}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Mousewheel, Keyboard]}
+                className="mySwiper mx-10 absolute bottom-32"
+            >
+                {TopArtist.map((item, index) => {
+                    return (
+                        <SwiperSlide key={index} onMouseEnter={() => onHover(item)}>
+                            <ArtistCard
+                                name={item.name}
+                                instagramProfileURL={item.instagramProfileURL}
+                                imageURL={item.imageURL}
+                                description={item.description}
+                            />
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+        </div>
     );
 }

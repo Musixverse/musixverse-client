a2ec2f9d63f47caadb64c16d2369ed0e2735a293
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-11-13 23:51:08 +0530

    updated responsiveness

--- src/components/HomePage/GetStartedAsArtist.js
+++ src/components/HomePage/GetStartedAsArtist.js
@@ -24,27 +24,27 @@ export default function GetStartedAsArtist(){
                 </div>
             </div>
 
-            <div className="bg-[#202020] rounded-[2.5rem] w-full mt-3 p-16 flex items-center justify-between">
-                <div className="flex leading-none">
-                    <h1 className="text-[54px] font-semibold mr-6">01</h1>
+            <div className="bg-[#202020] rounded-[2.5rem] w-full mt-3 p-16 flex flex-col md:flex-row space-y-14 md:space-y-0 items-center justify-between">
+                <div className="flex flex-col space-y-4 md:flex-row leading-none">
+                    <h1 className="text-5xl font-semibold mr-6">01</h1>
                     <div className="flex flex-col">
-                        <h2 className="font-semibold text-[28px] mb-3">Sign up as an Artist</h2>
+                        <h2 className="font-semibold text-2xl md:text-3xl mb-3">Sign up as an Artist</h2>
                         <p className="text-lg max-w-[340px]">Create an account and provide some basic information about yourself.</p>
                     </div>
                 </div>
 
-                <div className="flex leading-none">
-                    <h1 className="text-[54px] font-semibold mr-6">02</h1>
+                <div className="flex flex-col space-y-4 md:flex-row leading-none">
+                    <h1 className="text-5xl font-semibold mr-6">02</h1>
                     <div className="flex flex-col">
-                        <h2 className="font-semibold text-[28px] mb-3">Verify your profile</h2>
+                        <h2 className="font-semibold text-2xl md:text-3xl mb-3">Verify your profile</h2>
                         <p className="text-lg max-w-[340px]">Provide some additional details and get your profile verified.</p>
                     </div>
                 </div>
 
-                <div className="flex leading-none">
-                    <h1 className="text-[54px] font-semibold mr-6">03</h1>
+                <div className="flex flex-col space-y-4 md:flex-row leading-none">
+                    <h1 className="text-5xl font-semibold mr-6">03</h1>
                     <div className="flex flex-col">
-                        <h2 className="font-semibold text-[28px] mb-3">Launch your NFT</h2>
+                        <h2 className="font-semibold text-2xl md:text-3xl mb-3">Launch your NFT</h2>
                         <p className="text-lg max-w-[340px]">Create an NFT and offer it for sale on Musixverse.</p>
                     </div>
                 </div>
--- src/components/HomePage/GetStartedAsFan.js
+++ src/components/HomePage/GetStartedAsFan.js
@@ -33,27 +33,27 @@ export default function GetStartedAsFan(){
 
 
 
-            <div className="bg-[#202020] rounded-[2.5rem] w-full mt-3 p-16 flex items-center justify-between">
-                <div className="flex leading-none">
-                    <h1 className="text-[54px] font-semibold mr-6">01</h1>
+            <div className="bg-[#202020] rounded-[2.5rem] w-full mt-3 p-16 flex flex-col md:flex-row space-y-14 md:space-y-0 items-center justify-between">
+                <div className="flex flex-col space-y-4 md:flex-row leading-none">
+                    <h1 className="text-5xl font-semibold mr-6">01</h1>
                     <div className="flex flex-col">
-                        <h2 className="font-semibold text-[28px] mb-3">Sign up as a collector</h2>
+                        <h2 className="font-semibold text-2xl md:text-3xl mb-3">Sign up as a collector</h2>
                         <p className="max-w-sm text-lg">Register and start your journey on Musixverse.</p>
                     </div>
                 </div>
 
-                <div className="flex leading-none">
-                    <h1 className="text-[54px] font-semibold mr-6">02</h1>
+                <div className="flex flex-col space-y-4 md:flex-row leading-none">
+                    <h1 className="text-5xl font-semibold mr-6">02</h1>
                     <div className="flex flex-col">
-                        <h2 className="font-semibold text-[28px] mb-3"> Browse and buy NFTs</h2>
+                        <h2 className="font-semibold text-2xl md:text-3xl mb-3"> Browse and buy NFTs</h2>
                         <p className="max-w-sm text-lg">Look through the NFTs of your favorite artists and purchase the ones you like.</p>
                     </div>
                 </div>
 
-                <div className="flex leading-none">
-                    <h1 className="text-[54px] font-semibold mr-6">03</h1>
+                <div className="flex flex-col space-y-4 md:flex-row leading-none">
+                    <h1 className="text-5xl font-semibold mr-6">03</h1>
                     <div className="flex flex-col">
-                        <h2 className="font-semibold text-[28px] mb-3"> Enjoy the privileges</h2>
+                        <h2 className="font-semibold text-2xl md:text-3xl mb-3"> Enjoy the privileges</h2>
                         <p className="max-w-sm text-lg"> Enjoy exclusive privileges that come with being an NFT collector on Musixverse</p>
                     </div>
                 </div>

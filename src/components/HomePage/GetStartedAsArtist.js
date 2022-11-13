155c9504673f6e3871076f757e2939b3de239015
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-11-13 15:34:28 +0530

    coded get started as fan and artist

--- src/components/HomePage/GetStartedAsArtist.js
+++ src/components/HomePage/GetStartedAsArtist.js
@@ -1,12 +1,29 @@
 import Image from "next/image";
 import getStartedAsArtist from "../../../public/assets/homepage/getStartedAsArtist.png"
+// import get
+import styles from "../../../styles/HomePage/getStartedAsArtist.module.css";
 
 export default function GetStartedAsArtist(){
     return(
-        <div className="flex flex-col w-full">
-            <div className="w-full h-[510px] relative">
-                <Image alt="get started as artist" src={getStartedAsArtist} layout="responsive" priority/>
+        <div className="flex flex-col w-full pt-16">
+            <div className="relative rounded-[2.5rem]">
+                <div className="absolute z-[2] pl-36 h-[470px] w-[506px] flex top-[-31%]">
+                    <div className={"bg-transparent self-center " + styles['white-circle']}>
+                        <div className={"aspect-square rounded-full relative w-[296px] border-[#fff] border-[48px]"}></div>
+                    </div>
+                    <Image src={"/assets/homepage/artistCirclePpl.svg"} alt="musicians" priority objectFit="contain" layout="fill"/>
+                </div>
+                <div className="absolute w-full h-full items-center justify-end flex flex-col z-[1]">
+                    <h1 className="font-semibold opacity-[0.6] text-[74px] -mb-7 text-[#f2f2f2]">Get Started as<br/> an Artist</h1>
+                    <div className="backdrop-blur-[80px] backdrop-brightness-[0.5] rounded-b-[2.5rem] flex w-full p-9">
+                        <h4 className="font-semibold text-4xl text-[#f2f2f2] ml-auto">Get Started as an Artist</h4>
+                    </div>
+                </div>
+                <div className="w-full h-[358px] relative">
+                    <Image src={"/assets/homepage/artistBgFull.svg"} className="rounded-[2.5rem]" objectFit="cover" layout="fill" alt="background layer"/>
+                </div>
             </div>
+
             <div className="bg-[#202020] rounded-[2.5rem] w-full mt-3 p-16 flex items-center justify-between">
                 <div className="flex leading-none">
                     <h1 className="text-[54px] font-semibold mr-6">01</h1>
--- src/components/HomePage/GetStartedAsFan.js
+++ src/components/HomePage/GetStartedAsFan.js
@@ -1,12 +1,38 @@
 import Image from "next/image";
 import getStartedAsFan from "../../../public/assets/homepage/getStartedAsFan.png"
+import styles from "../../../styles/homePage/getStartedAsFan.module.css";
 
 export default function GetStartedAsFan(){
     return(
-        <div className="flex flex-col w-full pb-28">
-            <div className="w-full h-[510px] relative">
-                <Image alt="get started as artist" src={getStartedAsFan} layout="fill" priority/>
+        <div className="flex flex-col w-full py-36">
+            <div className="relative rounded-[2.5rem]">
+                <div className="absolute z-[2] pr-36 h-[470px] w-[506px] flex top-[-12%] right-0">
+                    <div className={"bg-transparent pt-6 " + styles['white-circle']}>
+                        <div className={"aspect-square rounded-full relative w-[296px] border-[#fff] border-[48px]"}></div>
+                    </div>
+                    <Image src={"/assets/homepage/fanCirclePpl.svg"} alt="musicians" priority objectFit="contain" layout="fill"/>
+                </div>
+                <div className="absolute w-full h-full items-center justify-end flex flex-col z-[1]">
+                    <h1 className="font-semibold opacity-[0.7] text-[74px] -mb-7 text-[#032C2D] mr-auto pl-32">Get Started as<br/> a Fan</h1>
+                    <div className="backdrop-blur-[80px] backdrop-brightness-50 rounded-b-[2.5rem] flex w-full p-9">
+                        <h4 className="font-semibold text-4xl text-[#f2f2f2] mr-auto">Get Started as a Fan</h4>
+                    </div>
+                </div>
+                <div className="w-full h-[358px] relative">
+                    <Image src={"/assets/homepage/fanBgFull.svg"} className="rounded-[2.5rem]" objectFit="cover" layout="fill" alt="background layer"/>
+                </div>
             </div>
+            {/* <div className="w-full h-[510px] relative">
+                <Image alt="get started as artist" src={getStartedAsFan} layout="fill" priority/>
+            </div> */}
+
+
+
+
+
+
+
+
             <div className="bg-[#202020] rounded-[2.5rem] w-full mt-3 p-16 flex items-center justify-between">
                 <div className="flex leading-none">
                     <h1 className="text-[54px] font-semibold mr-6">01</h1>

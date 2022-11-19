c9935ed7a3de18772c33fd4a5855c15fe9eb4ec7
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-11-19 17:22:27 +0530

    added clamp on hero

--- src/components/HomePage/NewHero.js
+++ src/components/HomePage/NewHero.js
@@ -2,6 +2,7 @@ import PromoNftCard from "./HomePageUtils/PromoNftCards";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../../../styles/HomePage/newHero.module.css";
 // import Carousel from "react-spring-3d-carousel";
 // import { Carousal } from "3d-react-carousal";
 
@@ -17,8 +18,8 @@ export default function NewHero(){
         <div className="flex flex-col items-center justify-between w-full md:flex-row pt-28">
             {/* Left Section */}
             <div className="flex flex-col">
-                <h1 className="font-semibold text-3xl leading-[3.5rem] lg:leading-normal lg:text-heroHeader1Lg xl:text-[3.5rem] 2xl:text-[4rem] text-[#54B902]">Create. Collect. Trade.</h1>
-                <h1 className="font-semibold text-4xl leading-[3.5rem] lg:leading-normal lg:text-heroHeader2Lg xl:text-[4rem] 2xl:text-[4.5rem]">Music Like Never <br/> Before</h1>
+                <h1 className={styles['hero-header1']}>Create. Collect. Trade.</h1>
+                <h1 className={styles['hero-header2']}>Music Like Never <br/> Before</h1>
                 {/* CTAs */}
                 <div className="flex items-center mt-8 space-x-3 lg:space-x-5 xl:mt-16">
                     {/* <Link href={"/mxcatalog/new-releases"} passHref> */}

c2b71e760f2aa0b3ba0d721eb2a140259ae4cde8
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-11-20 04:23:43 +0530

    final commit for hero

--- src/components/HomePage/HomePageUtils/ArtistIntro.js
+++ src/components/HomePage/HomePageUtils/ArtistIntro.js
@@ -1,14 +1,15 @@
-export default function ArtistIntro(){
+export default function ArtistIntro({artistName, isBand}){
     return (
-        <div className="p-5 flex flex-col items-center rounded-xl bg-light-100 dark:bg-[#2d2d2dfb]">
-            <div className="px-16 py-8">
-                <p>Artist</p>
-                <h1>Harry Arora</h1>
+        <div className="flex flex-col items-center p-5 rounded-3xl bg-light-100 dark:bg-[#2d2d2dfb] w-[380px]">
+            <div className="flex flex-col items-center mt-3">
+                <h3 className="font-medium text-[#868686] text-xs lg:text-lg 2xl:text-xl">{isBand? "Band":"Artist"}</h3>
+                <h1 className="text-lg font-semibold 2xl:text-2xl">{artistName}</h1>
             </div>
-
-            <div className="relative w-full">
-                <div className="backdrop-blur-[40px] w-full backdrop-brightness-[1.75] p-3 rounded-2xl flex items-center justify-between z-20"><p>Coming Soon</p></div>
-                <div className="absolute z-[-1] w-full h-9 bg-primary-600 rounded-xl top-5 md:top-[45%]"></div>
+            <div className="relative w-full mt-7">
+                <div className="absolute z-0 bottom-0 left-[50%] translate-x-[-50%] bg-primary-400 w-[150px] h-[35px]"></div>
+                <div className="py-6 backdrop-blur-[40px] backdrop-brightness-150 rounded-xl z-10 flex justify-center">
+                    <h3 className="text-lg font-semibold">Coming Soon</h3>
+                </div>
             </div>
         </div>
     );

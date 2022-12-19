bb705b4be077e04edc3dfe8de8bbc6e92e9ef183
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2022-12-19 22:03:39 +0530

    Corrected favorite spelling

--- src/components/Profile/FavouritesHeader.js
+++ src/components/Profile/FavouritesHeader.js
@@ -11,7 +11,7 @@ export default function FavouritesHeader() {
 			<div className={styles["filter-card__left-section"]}>
 				<Image src={theme === "dark" ? "/assets/record_w.svg" : "/assets/record_b.svg"} alt="vinyl disc" height={30} width={30}></Image>
 				<div className="flex-grow ml-4">
-					<h4 className={styles["filter-card__left-section--heading"]}>FAVOURITES</h4>
+					<h4 className={styles["filter-card__left-section--heading"]}>FAVORITES</h4>
 					{/* Horizontal separator */}
 					<div className="my-1 flex-grow border-t-[2px] border-[#818181]"></div>
 				</div>

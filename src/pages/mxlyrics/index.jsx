d06d881058d7a0dd80afbeef1ba66c81083b3282
Pushpit <pushpit07@gmail.com>
2022-12-28 12:47:52 +0530

    MxLyrics styles update
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/pages/mxlyrics/index.jsx
+++ src/pages/mxlyrics/index.jsx
@@ -67,7 +67,7 @@ const MxLyrics = () => {
 
 			<div className="flex items-center justify-center bg-light-200 dark:bg-dark-1000">
 				<div className="lg:flex-row flex-col flex w-full max-w-[1920px] pb-40 px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-36">
-					<div className="w-full gap-x-48 h-screen overflow-scroll no-scrollbar flex flex-col md:flex-row items-center justify-center m-0 p-0">
+					<div className="w-full gap-x-48 lg:h-screen overflow-scroll no-scrollbar flex flex-col md:flex-row items-center justify-center m-0 p-0">
 						<form
 							onSubmit={(e) => {
 								e.preventDefault();
@@ -77,16 +77,18 @@ const MxLyrics = () => {
 									callAutocompleteEndpoint();
 								}
 							}}
+							className="lg:mt-0 mt-40"
 						>
-							<p className="text-[105px] text-center font-bold -tracking-2">
-								M<span className="text-primary-400 text-[90px]">x</span> L<span className="text-[90px]">yrics</span>
+							<p className="sm:text-[105px] text-[70px] text-center font-bold -tracking-2">
+								M<span className="text-primary-400 text-[55px] sm:text-[90px]">x</span> L
+								<span className="text-[55px] sm:text-[90px]">yrics</span>
 							</p>
-							<h1 className="text-xl text-center font-semibold -mt-2">say goodbye to creative blocks</h1>
+							<h1 className="text-lg md:text-xl text-center font-semibold -mt-2">say goodbye to creative blocks</h1>
 
-							<div className="mt-16 flex justify-between text-sm">
+							<div className="mt-16 flex justify-between text-xs sm:text-sm">
 								<span
 									className={
-										"px-8 py-3 bg-primary-100 dark:bg-dark-800 rounded-full cursor-pointer border " +
+										"sm:px-8 sm:py-3 px-4 py-2 bg-primary-100 dark:bg-dark-800 rounded-full cursor-pointer border " +
 										(choice == "generateLyrics" ? "border-primary-400" : "border-transparent")
 									}
 									onClick={() => {
@@ -100,7 +102,7 @@ const MxLyrics = () => {
 								</span>
 								<span
 									className={
-										"px-8 py-3 bg-primary-100 dark:bg-dark-800 rounded-full cursor-pointer border " +
+										"sm:px-8 sm:py-3 px-4 py-2 bg-primary-100 dark:bg-dark-800 rounded-full cursor-pointer border " +
 										(choice == "autocompleteLyrics" ? "border-primary-400" : "border-transparent")
 									}
 									onClick={() => {
@@ -115,12 +117,12 @@ const MxLyrics = () => {
 							</div>
 
 							<div className="mt-10 flex flex-col justify-center items-center">
-								<div className="text-zinc-400">let us help you fastrack your music creation process</div>
+								<div className="text-[11px] sm:text-lg text-zinc-400">let us help you fastrack your music creation process</div>
 								{choice == "generateLyrics" ? (
 									<>
 										<textarea
 											rows={1}
-											className="mt-4 w-full flex gap-4 items-start justify-start placeholder-light-600 dark:placeholder-light-800 bg-light-300/50 dark:bg-dark-600 border border-transparent dark:border-dark-300 focus:outline-none focus:border-primary-500 dark:focus:border-primary-500 rounded-xl overflow-hidden px-5 py-4 resize-none"
+											className="mt-2 sm:mt-4 w-full flex gap-4 items-start justify-start placeholder-light-600 dark:placeholder-light-800 bg-light-300/50 dark:bg-dark-600 border border-transparent dark:border-dark-300 focus:outline-none focus:border-primary-500 dark:focus:border-primary-500 rounded-xl overflow-hidden px-5 py-4 resize-none"
 											placeholder="enter song name"
 											value={userInput}
 											onChange={(event) => setUserInput(event.target.value)}
@@ -139,7 +141,7 @@ const MxLyrics = () => {
 									<>
 										<textarea
 											rows={8}
-											className="mt-4 w-full flex gap-4 items-start justify-start placeholder-light-600 dark:placeholder-light-800 bg-light-300/50 dark:bg-dark-600 border border-transparent dark:border-dark-300 focus:outline-none focus:border-primary-500 dark:focus:border-primary-500 rounded-xl overflow-hidden px-5 py-4 resize-none"
+											className="mt-2 sm:mt-4 w-full flex gap-4 items-start justify-start placeholder-light-600 dark:placeholder-light-800 bg-light-300/50 dark:bg-dark-600 border border-transparent dark:border-dark-300 focus:outline-none focus:border-primary-500 dark:focus:border-primary-500 rounded-xl overflow-hidden px-5 py-4 resize-none"
 											placeholder="enter your lyrics"
 											value={userInput}
 											onChange={(event) => setUserInput(event.target.value)}
@@ -159,7 +161,7 @@ const MxLyrics = () => {
 						</form>
 
 						{apiOutput && (
-							<div className="mt-80 flex flex-col justify-center items-center">
+							<div className="mt-40 lg:mt-80 flex flex-col justify-center items-center">
 								<div className="flex font-tertiary text-5xl">
 									<h3>Output</h3>
 								</div>

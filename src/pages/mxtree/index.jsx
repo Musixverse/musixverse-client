a34d09de83ac03c85689e4d29f94103802e575dd
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2023-01-16 23:27:25 +0530

    minor style changes in Linktree

--- src/pages/mxtree/index.jsx
+++ src/pages/mxtree/index.jsx
@@ -10,8 +10,11 @@ const Mxtree = ({}) => {
 				<meta name="description" content={meta_description} />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-
-			<MxTree />
+			<div className="flex flex-col items-center justify-center w-full bg-light-200 dark:bg-dark-800">
+				<div className="w-full max-w-[1920px] py-36 px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-36">
+					<MxTree />
+				</div>
+			</div>		
 		</>
 	);
 };

053977eaf26a03eb6d2ec544a99788a8aa232fe9
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2023-01-19 10:57:59 +0530

    Added Privacy policy and Terms of Service pages

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -8,26 +8,32 @@ export default function PrivacyPolicy() {
 					<div className={styles["privacy_policy_heading"]}>Privacy Policy</div>
 				</div>
 				<div>
-					<div className="text-primary-500">(Last Updated November 3, 2021)</div>
+					<div className="text-primary-500">(Last Updated Jan 20, 2023)</div>
 				</div>
 
 				<div className="mt-5">
 					<div className={styles["privacy_policy_content"]}>
-						<span className="text-primary-500">Musixverse Technologies, Inc.</span>, as well as any present or future affiliated or related company
-						(“Musixverse”, “us” or “we”), recognizes the importance of your privacy. This privacy policy will help you understand how we use and
-						protect the data you provide us when you visit and use{" "}
+						Protecting your private information is our priority. This Statement of Privacy applies to
+						Musixverse Technologies Pvt. Ltd. (“Musixverse”) and {" "}
 						<a href={"/"} className="text-primary-500">
 							https://www.musixverse.com/
-						</a>{" "}
-						(&quot;website&quot;, &quot;service&quot;). We reserve the right to change this privacy policy at any given time, of which you will be
-						promptly updated. If you want to make sure that you are up to date with the latest changes, we advise you to frequently visit this page.
-						By using or visiting any Musixverse website (&quot;site&quot;), you understand and agree to the terms of this policy. Please read this
-						policy carefully.
+						</a>{" "} 
+						and governs data collection and usage. For the purposes of this Privacy Policy, unless otherwise noted, all
+						references to Musixverse include {" "}
+						<a href={"/"} className="text-primary-500">
+							https://www.musixverse.com/
+						</a>{" "} 
+						and Musixverse. For the purposes of this policy, {" "}
+						<a href={"/"} className="text-primary-500">
+							https://www.musixverse.com/
+						</a>{" "} 
+						will hereinafter be referred to as (“Site”). By using the
+						Site, you consent to the data practices described in this statement.
 					</div>
 				</div>
 
 				<div className="mt-10">
-					<div className={styles["privacy_policy_sub-heading"]}>What data do we collect?</div>
+					<div className={styles["privacy_policy_sub-heading"]}>Collection of your Personal Information</div>
 				</div>
 
 				<div className="mt-4">
@@ -64,55 +70,207 @@ export default function PrivacyPolicy() {
 				</div>
 
 				<div className="mt-10">
-					<div className={styles["privacy_policy_sub-heading"]}>What do we do with the information that we collect?</div>
+					<div className={styles["privacy_policy_sub-heading"]}>Use of your Personal Information</div>
 				</div>
 
 				<div className="mt-4">
 					<div className={styles["privacy_policy_content"]}>
-						Except as disclosed in this policy, Musixverse does not share or sell your personal information to any outside parties.
-						<br />
-						<br />
-						<span className="text-primary-500">a. </span>
-						Musixverse will use the personally identifiable information directly provided by you solely for the purpose for which you have provided
-						it, which may include: to respond to your comments and questions and to provide customer service; with your consent, to send you
-						information emails about products and services offered by us and our affiliates.
-						<br />
-						<br />
-						<span className="text-primary-500">b.</span> Disclosure: As a general rule, Musixverse will not disclose any of your personally
-						identifiable information except under one of the following circumstances: we have your permission, including the permission granted by
-						your acceptance of this policy; we determine in good faith that it is legally required to be revealed by any relevant statute,
-						regulation, ordinance, rule, administrative or court order, decree, or subpoena; it is information that we determine must be disclosed
-						to correct what we believe to be false or misleading information or to address activities that we believe to be manipulative, deceptive
-						or otherwise a violation of law; where we need to share your information to provide the product or service you have requested; when such
-						disclosure is made subject to confidentiality restrictions in connection with a sale, merger, transfer, exchange, or other disposition
-						(whether of assets, stock, or otherwise) of all or a portion of the business conducted by Musixverse.
+						Musixverse collects and uses your personal information to operate its Site(s) and deliver the
+						services you have requested. Musixverse may also use your personally identifiable information to
+						inform you of other products or services available from Musixverse and its affiliates.
 					</div>
 				</div>
 
 				<div className="mt-10">
-					<div className={styles["privacy_policy_sub-heading"]}>Links to other websites</div>
+					<div className={styles["privacy_policy_sub-heading"]}>Sharing Information with Third Parties</div>
 				</div>
 
 				<div className="mt-4">
 					<div className={styles["privacy_policy_content"]}>
-						Our website contains links that lead to other websites. If you click on these links Musixverse will not be held responsible for your
-						data and privacy protection. Visiting those websites is not governed by this privacy policy agreement. Make sure to read the privacy
-						policy documentation of the website you go to from our website.
+						Musixverse does not sell, rent or lease its customer lists to third parties. Musixverse may share
+						data with trusted partners to help perform statistical analysis, make payments, send you email or
+						postal mail, provide customer support, or arrange for deliveries. All such third parties are
+						prohibited from using your personal information except to provide these services to Musixverse,
+						and they are required to maintain the confidentiality of your information. Musixverse may disclose
+						your personal information, without notice, if required to do so by law or in the good faith belief
+						that such action is necessary to: (a) conform to the edicts of the law or comply with legal process
+						served on Musixverse or the Site; (b) protect and defend the rights or property of Musixverse;
+						and/or (c) act under exigent circumstances to protect the personal safety of users of Musixverse,
+						or the public.
 					</div>
 				</div>
 
 				<div className="mt-10">
-					<div className={styles["privacy_policy_sub-heading"]}>Safeguarding and Securing the data</div>
+					<div className={styles["privacy_policy_sub-heading"]}>International Transfers</div>
 				</div>
 
 				<div className="mt-4">
 					<div className={styles["privacy_policy_content"]}>
-						Musixverse (MXV) is committed to securing your data and keeping it confidential. We follow generally accepted industry standards to help
-						protect your personal information. No method of transmission over the internet, mobile technology, or method of electronic storage, is
-						completely secure. Musixverse (MXV) has done all in its power to prevent data theft, unauthorized access, and disclosure by implementing
-						the latest technologies and software, which help us safeguard all the information we collect online. If you have reason to believe that
-						your interaction with us is no longer secure (for example, if you feel that the security of any account you might have with us has been
-						compromised), please immediately notify us of the problem by contacting us at{" "}
+						Some of the parties identified above may be located in countries that do not provide an equivalent
+						level of protection as your home country. Where required, we have taken appropriate measures to
+						allow and secure the transfer of information about you to these recipients for the purposes
+						described above and in order to comply with local data privacy laws. By using some of our
+						services, and providing us information about you, you consent to the international transfer of
+						information about you to the above parties.
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>Automatically Collected Information</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						Information about your computer hardware and software may be automatically collected by the
+						Site. This information can include: your IP address, browser type, domain names, access
+						times and referring website addresses. This information is used for the operation of the service,
+						to maintain quality of the service, and to provide general statistics regarding use of the Site(s).
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>Use of Cookies</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						The Site may use (&quot;cookies&quot;) to help you personalize your online experience. A cookie is a text file
+						that is placed on your hard disk by a web page server. Cookies cannot be used to run programs or
+						deliver viruses to your computer. Cookies are uniquely assigned to you, and can only be read by a
+						web server in the domain that issued the cookie to you. One of the primary purposes of cookies is
+						to provide a convenience feature to save you time. The purpose of a cookie is to tell the Web server
+						that you have returned to a specific page. For example, if you personalize Musixverse pages, or
+						register with Musixverse site or services, a cookie helps Musixverse to recall your specific
+						information on subsequent visits. This simplifies the process of recording your personal
+						information, such as billing addresses, shipping addresses, and so on. When you return to the same
+						Musixverse website, the information you previously provided can be retrieved, so you can easily
+						use the Musixverse features that you customized. You have the ability to accept or decline cookies.
+						Most web browsers automatically accept cookies, but you can usually modify your browser setting
+						to decline cookies if you prefer. If you choose to decline cookies, you may not be able to fully
+						experience the interactive features of the Musixverse services or websites you visit.
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>Links</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						The Site contains links to other sites. Please be aware that we are not responsible for the content
+						or privacy practices of such other sites. We encourage our users to be aware when they leave our
+						site and to read the privacy statements of any other site that collects personally identifiable
+						information.
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>Right to Deletion</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						Subject to certain exceptions set out below, on receipt of a verifiable request from you, we will:
+						• Delete your personal information from our records; and
+						• Direct any service providers to delete your personal information from their records.
+						<br />
+						<br />
+						Please note that we may not be able to comply with requests to delete your personal information
+						if it is necessary to:
+						• Complete the transaction for which the personal information was collected, fulfill the terms of a
+						written warranty or product recall conducted in accordance with the law, provide a good or service
+						requested by you, or reasonably anticipated within the context of our ongoing business relationship
+						with you, or otherwise perform a contract between you and us;
+						• Detect security incidents, protect against malicious, deceptive, fraudulent, or illegal activity; or
+						prosecute those responsible for that activity;
+						• Debug to identify and repair errors that impair existing intended functionality;
+						• Exercise free speech, ensure the right of another consumer to exercise his or her right of free
+						speech, or exercise another right provided for by law;
+						• Enable solely internal uses that are reasonably aligned with your expectations based on
+						your relationship with us;
+						• Comply with an existing legal obligation; or
+						• Otherwise use your personal information, internally, in a lawful manner that is compatible with
+						the context in which you provided the information.
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>Children Under Thirteen</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						Musixverse does not knowingly collect personally identifiable information from children under
+						the age of thirteen. If you are under the age of thirteen, you must ask your parent or guardian for
+						permission to use the Site and its allied products. We will take appropriate steps to delete any
+						Personally Identifiable Information of persons less than 13 years of age that has been collected on
+						our Site without verified parental consent upon learning of the existence of such Personally
+						Identifiable Information.
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>E-mail Communications</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						From time to time, Musixverse may contact you via email for the purpose of providing
+						announcements, promotional offers, alerts, confirmations, surveys, and/or other general
+						communication. If you would like to stop receiving marketing or promotional communications via
+						email from Musixverse, you may opt out of such communications by clicking on the
+						UNSUBSCRIBE button.
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>Changes to this Statement</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						Musixverse reserves the right to change this Privacy Policy from time to time. We will notify you
+						about significant changes in the way we treat personal information by sending a notice to the
+						primary email address specified in your account, by placing a prominent notice on our site, and/or
+						by updating any privacy information on this page. Your continued use of the Site and/or services
+						available through this Site after such modifications will constitute your: (a) acknowledgment of
+						the modified Privacy Policy; and (b) agreement to abide and be bound by that Policy.
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>Dispute resolution</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						You agree to the dispute resolution mechanism as the sole means to seek resolution to any disputes
+						arising from this Privacy Policy available at []
+					</div>
+				</div>
+
+				<div className="mt-10">
+					<div className={styles["privacy_policy_sub-heading"]}>Contact Information</div>
+				</div>
+
+				<div className="mt-4">
+					<div className={styles["privacy_policy_content"]}>
+						Musixverse welcomes your questions or comments regarding this Statement of Privacy. If
+						you believe that Musixverse has not adhered to this Statement, please contact Musixverse
+						at:
+						<a
+							href="https://www.google.co.in/maps/search/KH.No.+23%2F2,1st+Floor,Gali+No.5,+Block-A,+Himgiri+Enclave,+Village+Mukandpur,+Delhi,+North+%09%09%09%09%09%09East,+Delhi,+India,+110084/@28.6915721,77.2055784,21z?hl=en&authuser=0"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="font-primary font-semibold dark:text-[#afafaf]"
+						>
+						{" "}
+							Musixverse Technologies Pvt Ltd.
+							KH.No. 23/2,1st Floor,Gali No.5, Block-A, Himgiri Enclave, Village Mukandpur, Delhi, North
+							East, Delhi, India, 110084
+						{" "}
+						</a>	
+						Email Address:
 						<a
 							href="mailto:contact@musixverse.com"
 							target="_blank"
@@ -121,39 +279,14 @@ export default function PrivacyPolicy() {
 						>
 							contact@musixverse.com
 						</a>
-					</div>
-				</div>
-
-				<div className="mt-10">
-					<div className={styles["privacy_policy_sub-heading"]}>Changes to our Privacy Policy</div>
-				</div>
-
-				<div className="mt-4">
-					<div className={styles["privacy_policy_content"]}>
-						We reserve the right to update the terms of this privacy policy at any time whenever necessary to reflect customer feedback and changes
-						to our platform.{" "}
-						<span className="text-primary-500">When we make changes, we will revise the “last updated” date at the top of the policy.</span> If
-						there are material changes to this policy or in how we will use your personal information, we will notify you by prominently posting a
-						notice of such changes here or on our home page. We encourage you to periodically review this policy whenever you visit our website.
-					</div>
-				</div>
-
-				<div className="mt-10">
-					<div className={styles["privacy_policy_sub-heading"]}>Contact Us</div>
-				</div>
-
-				<div className="mt-4">
-					<div className={styles["privacy_policy_content"]}>
-						For general inquires or communications, or if you have a technical or support question, please send us an email at 
 						<a
-							href="mailto:contact@musixverse.com"
+							href="tel:8171648640"
 							target="_blank"
 							rel="noopener noreferrer"
 							className="font-primary font-semibold dark:text-[#afafaf]"
 						>
-							contact@musixverse.com
-						</a>
-						. We will respond to any questions or concerns within 30 days.
+							+91 81716 48640
+						</a>	
 					</div>
 				</div>
 

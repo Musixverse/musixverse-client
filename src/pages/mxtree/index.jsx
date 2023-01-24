a944ab6813db42aa66808cc3d8861b65e1105f79
Pushpit <pushpit07@gmail.com>
2023-01-24 20:55:36 +0530

    Updated folder names
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/pages/mxtree/index.jsx
+++ src/pages/mxtree/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { meta_description } from "../../config/constants";
-import MxTree from "../../components/Linktree/mxtree";
+import MxTree from "../../components/MxTree/MxTree";
 
 const Mxtree = ({}) => {
 	return (
@@ -10,11 +10,12 @@ const Mxtree = ({}) => {
 				<meta name="description" content={meta_description} />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
 			<div className="flex flex-col items-center justify-center w-full bg-light-200 dark:bg-dark-800">
 				<div className="w-full max-w-[1920px] py-36 px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-36">
 					<MxTree />
 				</div>
-			</div>		
+			</div>
 		</>
 	);
 };

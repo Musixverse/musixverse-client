c66ffb24c9797aec7b1428435cf4ef8f1d41dd50
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2023-01-25 22:14:46 +0530

    minor image responsive changes

--- src/components/Blogs/BlogImage.js
+++ src/components/Blogs/BlogImage.js
@@ -7,8 +7,8 @@ import Image from 'next/image'
 
 const BlogImage = ({src,alt,width,height}) => {
   return (
-    <div className='flex justify-center py-8'>
-        <Image src={src} alt={alt} width={width} height={height} objecfit={'contain'} />
+    <div className='flex relative justify-center py-8 w-[768px]'>
+        <Image src={src} alt={alt} layout={"fill"} objecfit={'contain'} />
     </div>
   )
 }

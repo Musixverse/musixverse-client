aacf87f8b84430a5cae17e9a3c34e207b280f6ad
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2023-01-27 11:45:46 +0530

    blog image fixes

--- src/components/Blogs/BlogImage.js
+++ src/components/Blogs/BlogImage.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 
-// @sparshsharma2510
-//width - 768px (input)
-// height - auto adjust according to image dimensions
-
 const BlogImage = ({src,alt,width,height}) => {
   return (
-    <div className='flex relative justify-center py-8 w-[768px]'>
-        <Image src={src} alt={alt} layout={"fill"} objecfit={'contain'} />
+    <div className='flex relative justify-center py-8'>
+        <Image src={src} alt={alt} width={width} height={height} priority />
     </div>
   )
 }

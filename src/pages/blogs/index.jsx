b37ffa8effcc7187281e0dc0a383f0e702c368d3
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2023-01-27 11:49:37 +0530

    added responsiveness to blog layout component

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -12,7 +12,7 @@ const Blogs = ({}) => {
 			</Head>
 
 			<div className="flex flex-col items-center justify-center w-full bg-light-200 dark:bg-dark-800">
-				<div className="overflow-x-hidden justify-center w-full max-w-[768px]">
+				<div className="overflow-x-hidden justify-center w-full max-w-[768px] px-6 md:px-8 lg:px-0">
 					<Blog />
 				</div>
 			</div>

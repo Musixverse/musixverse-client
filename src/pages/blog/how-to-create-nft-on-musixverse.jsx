45f81ea97360bc13162f6a57bd89c3ec2b8c4ff5
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2023-02-12 16:19:26 +0530

    added blog pages with blog/<blog>

---
+++ src/pages/blog/how-to-create-nft-on-musixverse.jsx
@@ -0,0 +1,23 @@
+import Blog from "@/components/Blog/BlogPages/create-nft";
+import Head from "next/head";
+import { meta_description } from "@/config/constants";
+
+const HowToCreateNFT = ({}) => {
+	return (
+		<>
+			<Head>
+				<title>Musixverse | How to create NFT on Musixverse?</title>
+				<meta name="description" content={meta_description} />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div className="flex flex-col items-center justify-center w-full bg-light-200 dark:bg-dark-800">
+				<div className="overflow-x-hidden justify-center w-full max-w-[768px] px-6 md:px-8 lg:px-0">
+					<Blog />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default HowToCreateNFT;
---
+++ src/pages/blog/how-to-setup-cryptowallet.jsx
@@ -0,0 +1,23 @@
+import Blog from "@/components/Blog/Blog";
+import Head from "next/head";
+import { meta_description } from "@/config/constants";
+
+const HowToSetupCryptoWallet = ({}) => {
+	return (
+		<>
+			<Head>
+				<title>Musixverse | How to setup a Crypto Wallet?</title>
+				<meta name="description" content={meta_description} />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div className="flex flex-col items-center justify-center w-full bg-light-200 dark:bg-dark-800">
+				<div className="overflow-x-hidden justify-center w-full max-w-[768px] px-6 md:px-8 lg:px-0">
+					<Blog />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default HowToSetupCryptoWallet;
